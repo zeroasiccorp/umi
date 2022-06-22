76fa70d09745ba0306847548dec88a8a5d0ecf3f
aolofsson <andreas@zeroasic.com>
2022-06-21 22:15:08 -0400

    Adding wavedrom based timing diagram

---
+++ docs/umi_waveforms.py
@@ -0,0 +1,14 @@
+import wavedrom
+
+######################
+# READY VALID
+######################
+svg = wavedrom.render("""
+{ "signal": [
+ { "name": "clk",    "wave": "P........", period=2},
+ { "name": "valid_out",  "wave": "0.1.01..0"},
+ { "name": "packet_out", "wave": "x.23x4.5x", "data": "P0 P1 P2 P3"},
+ { "name": "ready_in",  "wave": "1....01.."},
+
+]}""")
+svg.saveas("ready_valid.svg")
