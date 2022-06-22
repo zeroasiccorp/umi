4703b52d3d33e49b77b3afc2deee4632f8e510e2
aolofsson <andreas@zeroasic.com>
2022-06-21 21:31:40 -0400

    Implementing first version of umi pack/unpack

--- umi/rtl/umi_pack.v
+++ umi/rtl/umi_pack.v
@@ -11,16 +11,47 @@ module umi_pack
   #(parameter AW = 64,
     parameter PW = 256)
    (
-    //Command Inputs
-    input [7:0]      opcode_in,
-    input [3:0]      size_in,// number of bytes to transfer
-    input [19:0]     user_in, //user control field
-    //Address/Data
-    input [AW-1:0]   dstaddr_in, //destination address
-    input [AW-1:0]   srcaddr_in, //source address (for reads)
-    input [4*AW-1:0] data_in, //data
-    //Output packet
+    // Command inputs
+    input [7:0]      opcode,
+    input [3:0]      size,// number of bytes to transfer
+    input [19:0]     user, // user control field
+    input 	     burst, // active burst in process
+    // Address/Data
+    input [AW-1:0]   dstaddr, // destination address
+    input [AW-1:0]   srcaddr, // source address (for reads/atomics)
+    input [4*AW-1:0] data, // data
+    // Output packet
     output [PW-1:0]  packet_out
     );
+
+   wire [31:0] 	     cmd_out;
+   wire [255:0]      data_out;
+
+   // dommand packer
+   assign cmd_out[7:0]   = opcode[7:0];
+   assign cmd_out[11:8]  = size[3:0];
+   assign cmd_out[31:12] = user[19:0];
+
+   // data/address packer
+   generate
+      if(AW==64 & PW==256) begin : p256
+	 assign cmd_read       = ~burst & cmd_read;
+	 // see README to understand why...
+	 assign data_out[255:0] = {data[159:0], data[255:160]};
+	 // driving data baseed on transaction type
+	 assign packet_out[31:0]    = burst ? data_out[31:0]  : cmd_out[31:0];
+	 assign packet_out[63:32]   = burst ? data_out[63:32] : dstaddr[31:0];
+	 assign packet_out[95:64]   = burst ? data_out[95:65] : srcaddr[31:0];
+	 assign packet_out[191:96]  = data_out[191:96];
+	 assign packet_out[223:192] = cmd_read ? srcaddr[63:32] : data_out[223:192];
+	 assign packet_out[255:224] = burst ? data_out[255:224] : dstaddr[63:32];
+      end
+   endgenerate
+
+   // decoding signals needed for packet mux
+   umi_decode umi_decode (// input
+			  .cmd      (cmd_out[31:0]),
+			  // output
+			  .cmd_read (cmd_read));
 
 endmodule // umi_pack
