7a7b4eb63c0efecbd6c69aa10d309c12d862f3f7
Andreas.Olofsson <andreas.d.olofsson@gmail.com>
2022-10-20 15:03:51 -0400

    Expanding decode interface for umi packer  (avoids linter issues and forward proofs)

--- umi/rtl/umi_pack.v
+++ umi/rtl/umi_pack.v
@@ -27,7 +27,7 @@ module umi_pack
    wire [31:0] 	     cmd_out;
    wire [255:0]      data_out;
 
-   // dommand packer
+   // command packer
    assign cmd_out[7:0]   = opcode[7:0];
    assign cmd_out[11:8]  = size[3:0];
    assign cmd_out[31:12] = user[19:0];
@@ -48,10 +48,46 @@ module umi_pack
       end
    endgenerate
 
-   // decoding signals needed for packet mux
-   umi_decode umi_decode (// input
-			  .cmd      (cmd_out[31:0]),
-			  // output
-			  .cmd_read (cmd_read));
+   wire			cmd_atomic;
+   wire			cmd_atomic_add;
+   wire			cmd_atomic_and;
+   wire			cmd_atomic_max;
+   wire			cmd_atomic_min;
+   wire			cmd_atomic_or;
+   wire			cmd_atomic_swap;
+   wire			cmd_atomic_xor;
+   wire			cmd_invalid;
+   wire [7:0]		cmd_opcode;
+   wire [3:0]		cmd_size;
+   wire [19:0]		cmd_user;
+   wire			cmd_write;
+   wire			cmd_write_ack;
+   wire			cmd_write_normal;
+   wire			cmd_write_response;
+   wire			cmd_write_signal;
+   wire			cmd_write_stream;
+
+   umi_decode umi_decode (.cmd			(cmd_out[31:0]),
+			  /*AUTOINST*/
+			  // Outputs
+			  .cmd_invalid		(cmd_invalid),
+			  .cmd_write		(cmd_write),
+			  .cmd_read		(cmd_read),
+			  .cmd_atomic		(cmd_atomic),
+			  .cmd_write_normal	(cmd_write_normal),
+			  .cmd_write_signal	(cmd_write_signal),
+			  .cmd_write_ack	(cmd_write_ack),
+			  .cmd_write_stream	(cmd_write_stream),
+			  .cmd_write_response	(cmd_write_response),
+			  .cmd_atomic_swap	(cmd_atomic_swap),
+			  .cmd_atomic_add	(cmd_atomic_add),
+			  .cmd_atomic_and	(cmd_atomic_and),
+			  .cmd_atomic_or	(cmd_atomic_or),
+			  .cmd_atomic_xor	(cmd_atomic_xor),
+			  .cmd_atomic_min	(cmd_atomic_min),
+			  .cmd_atomic_max	(cmd_atomic_max),
+			  .cmd_opcode		(cmd_opcode[7:0]),
+			  .cmd_size		(cmd_size[3:0]),
+			  .cmd_user		(cmd_user[19:0]));
 
 endmodule // umi_pack
