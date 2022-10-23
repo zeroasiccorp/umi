39362b5cae29a1eb817d417e0e8082921ed33b96
aolofsson <andreas@zeroasic.com>
2022-10-22 21:09:10 -0400

    Rewriting unpack for clarity - Read/write major network selector now pushed to bit 0 - Renaming signals for clarity - Adding initial docs to show read/write alloaction.

--- umi/rtl/umi_unpack.v
+++ umi/rtl/umi_unpack.v
@@ -5,6 +5,19 @@
  *
  * Documentation:
  *
+ * Higher priority write/response (packet[0]==1)
+ *
+ * 1. stores
+ * 2. read (load) responses
+ * 3. atomic response
+ * 4. acks
+ * 5. other responses
+ *
+ * Lower priority read/request (packet[0]==0)
+ *
+ * 1. loads
+ * 2. atomic request
+ * 3. stores/writes that need acks
  *
  ******************************************************************************/
 module umi_unpack
@@ -13,17 +26,24 @@ module umi_unpack
    (
     // Input packet
     input [UW-1:0]    packet,
+    // Control
+    output 	      write, // write transaction
+    output [6:0]      command, // raw opcode
+    output [3:0]      size, // transaction size
+    output [19:0]     options, // raw command
     //Address/Data
     output [AW-1:0]   dstaddr, // read/write target address
     output [AW-1:0]   srcaddr, // read return address
-    output [4*AW-1:0] data, // write data
-    output [31:0]     cmd // raw command
+    output [4*AW-1:0] data // write data
     );
 
    // data field unpacker
    generate
       if(AW==64 & UW==256) begin : p256
-	 assign cmd[31:0]       = packet[31:0];
+	 assign write           = packet[0];
+	 assign command[6:0]    = packet[7:1];
+	 assign size[3:0]       = packet[11:8];
+	 assign options[19:0]   = packet[31:12];
 	 assign dstaddr[31:0]   = packet[63:32];
 	 assign dstaddr[63:32]  = packet[255:224];
 	 assign srcaddr[31:0]   = packet[95:64];
