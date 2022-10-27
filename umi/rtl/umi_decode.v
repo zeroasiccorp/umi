011e394200f9ce6cb3ba3810c4f54c704b2b870e
aolofsson <andreas@zeroasic.com>
2022-10-27 15:35:01 -0400

    Fixing broken decode - Initial testing was only doing simple reads/writes which only relies on bit [0] (write signal)

--- umi/rtl/umi_decode.v
+++ umi/rtl/umi_decode.v
@@ -32,29 +32,31 @@ module umi_decode
    output      cmd_atomic_maxu
    );
 
+   wire [7:0]  opcode = {command[6:0], write};
+
 `include "umi_messages.vh"
 
    // Command grouping
-   assign cmd_invalid        = (command[7:0]==INVALID);
-   assign cmd_read_request   = (command[7:0]==READ_REQUEST);
-   assign cmd_atomic         = (command[3:0]==ATOMIC);
+   assign cmd_invalid        = (opcode[7:0]==INVALID);
+   assign cmd_read_request   = (opcode[7:0]==READ_REQUEST);
+   assign cmd_atomic         = (opcode[3:0]==ATOMIC);
 
    // Write controls
-   assign cmd_write_posted   = (command[7:0]==WRITE_POSTED);
-   assign cmd_write_response = (command[7:0]==WRITE_RESPONSE);
-   assign cmd_write_signal   = (command[7:0]==WRITE_SIGNAL);
-   assign cmd_write_stream   = (command[7:0]==WRITE_STREAM);
-   assign cmd_write_ack      = (command[7:0]==WRITE_ACK);
+   assign cmd_write_posted   = (opcode[7:0]==WRITE_POSTED);
+   assign cmd_write_response = (opcode[7:0]==WRITE_RESPONSE);
+   assign cmd_write_signal   = (opcode[7:0]==WRITE_SIGNAL);
+   assign cmd_write_stream   = (opcode[7:0]==WRITE_STREAM);
+   assign cmd_write_ack      = (opcode[7:0]==WRITE_ACK);
 
    // Atomics
-   assign cmd_atomic_add     = (command[7:0]==ATOMIC_ADD);
-   assign cmd_atomic_and     = (command[7:0]==ATOMIC_AND);
-   assign cmd_atomic_or      = (command[7:0]==ATOMIC_OR);
-   assign cmd_atomic_xor     = (command[7:0]==ATOMIC_XOR);
-   assign cmd_atomic_max     = (command[7:0]==ATOMIC_MAX);
-   assign cmd_atomic_min     = (command[7:0]==ATOMIC_MIN);
-   assign cmd_atomic_maxu    = (command[7:0]==ATOMIC_MAXU);
-   assign cmd_atomic_minu    = (command[7:0]==ATOMIC_MINU);
-   assign cmd_atomic_swap    = (command[7:0]==ATOMIC_SWAP);
+   assign cmd_atomic_add     = (opcode[7:0]==ATOMIC_ADD);
+   assign cmd_atomic_and     = (opcode[7:0]==ATOMIC_AND);
+   assign cmd_atomic_or      = (opcode[7:0]==ATOMIC_OR);
+   assign cmd_atomic_xor     = (opcode[7:0]==ATOMIC_XOR);
+   assign cmd_atomic_max     = (opcode[7:0]==ATOMIC_MAX);
+   assign cmd_atomic_min     = (opcode[7:0]==ATOMIC_MIN);
+   assign cmd_atomic_maxu    = (opcode[7:0]==ATOMIC_MAXU);
+   assign cmd_atomic_minu    = (opcode[7:0]==ATOMIC_MINU);
+   assign cmd_atomic_swap    = (opcode[7:0]==ATOMIC_SWAP);
 
 endmodule // umi_decode
