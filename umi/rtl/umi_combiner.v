4338fdac8aba0e635b7daaeeb82468dceba86112
aolofsson <andreas@zeroasic.com>
2022-10-26 21:20:19 -0400

    Adding ready signals for combiner

--- umi/rtl/umi_combiner.v
+++ umi/rtl/umi_combiner.v
@@ -19,20 +19,24 @@ module umi_combiner
    ( // Input (0), Higher Priority
     input 	    umi0_in_valid,
     input [UW-1:0]  umi0_in_packet,
+    output 	    umi0_in_ready,
     // Input (1)
     input 	    umi1_in_valid,
     input [UW-1:0]  umi1_in_packet,
     output 	    umi1_in_ready,
     // Output
     output 	    umi_out_valid,
-    output [UW-1:0] umi_out_packet
+    output [UW-1:0] umi_out_packet,
+    input 	    umi_out_ready
     );
 
-   //never waits
-   wire 	    umi0_in_ready;
+   // local wires
+   wire 	    umi0_ready;
+   wire 	    umi1_ready;
+
    umi_mux #(.N(2))
    umi_mux (// Outputs
-	    .umi_in_ready	({umi1_in_ready,umi0_in_ready}),
+	    .umi_in_ready	({umi1_ready,umi0_ready}),
 	    .umi_out_valid	(umi_out_valid),
 	    .umi_out_packet	(umi_out_packet[UW-1:0]),
 	    // Inputs
@@ -42,5 +46,9 @@ module umi_combiner
 	    .mask		(2'b00),
 	    .umi_in_valid	({umi1_in_valid, umi0_in_valid}),
 	    .umi_in_packet	({umi1_in_packet, umi0_in_packet}));
+
+   // Flow through pushback
+   assign umi0_in_ready = umi_out_ready & umi0_ready;
+   assign umi1_in_ready = umi_out_ready & umi1_ready;
 
 endmodule // umi_splitter
