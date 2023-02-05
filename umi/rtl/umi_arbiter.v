0ecd6fec0d94a2483b3774b89eba62ea050f8eea
aolofsson <andreas@zeroasic.com>
2023-02-04 21:00:00 -0500

    Refactoring umi_arbiter for clarity

--- umi/rtl/umi_arbiter.v
+++ umi/rtl/umi_arbiter.v
@@ -15,34 +15,29 @@
  *
  ******************************************************************************/
 module umi_arbiter
-  #(parameter UW  = 256, // UMI transaction width
-    parameter N   = 4    // number of inputs
+  #(parameter N   = 4  // number of inputs
     )
    (// controls
-    input 	   clk,
-    input 	   nreset,
-    input [1:0]    mode, // arbiter mode
-    input [N-1:0]  mask, // 1=disable input request,0=enable inputn request
-    // Incoming UMI request
-    input [N-1:0]  umi_in_valid,
-    output [N-1:0] umi_in_ready,
-    // Outgoing UMI response
-    output [N-1:0] umi_out_valid
+    input 	       clk,
+    input 	       nreset,
+    input [1:0]        mode, // [00]=priority,[01]=roundrobin,[1x]=reserved
+    input [N-1:0]      mask, // 1 = disable request, 0 = enable request
+    input [N-1:0]      requests, // incoming requests
+    output reg [N-1:0] grants  // outgoing grants
     );
 
    wire 	       collision;
    reg [N-1:0] 	       thermometer;
    wire [N-1:0]        spec_requests[0:N-1];
    wire [N-1:0]        spec_grants[0:N-1];
-   reg [N-1:0] 	       grants;
    genvar 	       i;
 
-   // Thermometer mask that gets hotter with every
-   // collision and wraps on all ones.
+   // Thermometer mask that gets hotter with every collision
+   // wraps to zero when all ones
    always @ (posedge clk or negedge nreset)
      if (~nreset)
        thermometer[N-1:0] <= {N{1'b0}};
-     else if(collision & (mode[1:0]!=00))
+     else if(collision & (mode[1:0]==2'b10))
        thermometer[N-1:0] <= (&thermometer[N-2:0]) ? 'b0 : {thermometer[N-2:0],1'b1};
 
    // 1. Create N rotated set of requests
@@ -52,14 +47,14 @@ module umi_arbiter
 	// double width needed for rotation
 	assign spec_requests[i] = ~mask[N-1:0] &
 				  ~thermometer[N-1:0] &
-				  umi_in_valid[N-1:0];
+				   requests[N-1:0];
 
 	// Priority Slection Using Masked Inputs
 	umi_priority #(.N(N))
 	umi_prioroty(// Outputs
-		     .umi_out_valid (spec_grants[i][N-1:0]),
+		     .grants   (spec_grants[i][N-1:0]),
 		     // Inputs
-		     .umi_in_valid  (spec_requests[i][N-1:0]));
+		     .requests (spec_requests[i][N-1:0]));
      end
 
    // Or together all grants
@@ -71,14 +66,7 @@ module umi_arbiter
 	  grants[N-1:0] = grants[N-1:0] | spec_grants[i][N-1:0];
      end
 
-   // Valid==Grant
-   assign umi_out_valid[N-1:0] = grants[N-1:0];
-
-   // Drive ready low on request and ~grant
-   assign umi_in_ready[N-1:0] = ~umi_in_valid[N-1:0] |
-				(umi_in_valid[N-1:0] & grants[N-1:0]);
-
    // Detect collision on pushback
-   assign collision = |(umi_in_valid[N-1:0] & ~grants[N-1:0]);
+   assign collision = |(requests[N-1:0] & ~grants[N-1:0]);
 
 endmodule
--- umi/rtl/umi_priority.v
+++ umi/rtl/umi_priority.v
@@ -11,23 +11,21 @@
 module umi_priority
   #(parameter N   = 4    // number of inputs
     )
-   (// Inputs Valids (requests)
-     input [N-1:0]  umi_in_valid,
-    // Output Valids (grants)
-     output [N-1:0] umi_out_valid
+   (input [N-1:0]  requests, // request vector ([0] is highest priority)
+    output [N-1:0] grants    // outgoing grant vector
     );
 
    wire [N-1:0]   mask;
    genvar 	  j;
 
-   // priority mask
+   // priority maskx
    assign mask[0] = 1'b0;
    for (j=N-1; j>=1; j=j-1)
      begin : ipri
-	assign mask[j] = |umi_in_valid[j-1:0];
+	assign mask[j] = |requests[j-1:0];
      end
 
-   //grant circuit
-   assign umi_out_valid[N-1:0] = umi_in_valid[N-1:0] & ~mask[N-1:0];
+   // priority grant circuit
+   assign grants[N-1:0] = requests[N-1:0] & ~mask[N-1:0];
 
 endmodule // umi_priority
