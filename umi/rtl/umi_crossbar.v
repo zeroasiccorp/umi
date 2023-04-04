899fb574befa4102bb62121bdf8b5a2b4511f47b
aolofsson <andreas@zeroasic.com>
2023-04-04 11:46:59 -0400

    First draft of umi crossbar

--- umi/rtl/umi_crossbar.v
+++ umi/rtl/umi_crossbar.v
@@ -5,42 +5,33 @@
  *
  * Documentation:
  *
- * Simple N x N crossbar with one-hot select matrix.
- * 
- * 
- * 
+ * N x N crossbar with one-hot selects.
+ *
  ******************************************************************************/
 module umi_crossbar
   #(parameter TARGET = "DEFAULT", // implementation target
     parameter UW     = 256,       // UMI width
-    parameter N      = 1          // UMI ports          
+    parameter N      = 1          // UMI ports
     )
    (
-    input [UW*N-1:0] 	 in, // input ports
-    input [N*N-1:0] 	 sel, // one hot selects ([N-1:0]=output port 0)
-    output reg [UW*N-1:0] out  // output ports  
+    input [UW*N-1:0]  in, // input ports
+    input [N*N-1:0]   sel, // one hot selects ([N-1:0]=output port 0)
+    output [UW*N-1:0] out  // output ports
     );
 
-   for (i=0;i<N;i=i+1)
-     begin
-	out[i*UW+:UW] = 'b0;
-	for (j=0;j<N;j=j+1)
-	  out[i*UW+:UW] = out[i*UW+:UW] | (in[i*UW+:UW] & sel)
-							      
-	       
-	
+   genvar i;
+   generate
+     for(i=0;i<N;i=i+1)
+       begin: imux
+	  la_mux #(.DW(UW),
+		   .N(N))
+	  la_mux(// Outputs
+		 .out	(out[i*UW+:UW]),
+		 // Inputs
+		 .sel	(sel[i*N+:N]),
+		 .in	(in[UW*N-1:0]));
 
-sel[i*N];
-	
-	  end
-     begin
-
-     end
-   
-
+       end
+   endgenerate
 
 endmodule // umi_crossbar
-	
-// Local Variables:
-// verilog-library-directories:("." "../../../lambdalib/ramlib/rtl")
-// End:
