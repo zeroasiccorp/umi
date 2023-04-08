9ddc2d4892a6c5a90b4ea2e3ecb5971a48e8a46c
aolofsson <andreas@zeroasic.com>
2023-04-08 18:48:24 -0400

    Adding light weight umi address field extractor - Common operations in decode, less code bloat

---
+++ umi/rtl/umi_addr.v
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Function:  UMI Packet Address Field Extractor
+ * Author:    Andreas Olofsson
+ * License:
+ *
+ ******************************************************************************/
+module umi_addr
+  #(parameter AW = 64,
+    parameter UW = 256
+    )
+   (
+    input [UW-1:0]  packet, // full packet
+    output [AW-1:0] dstaddr // read/write target address
+    );
+
+   umi_unpack #(.UW(UW))
+   umi_unpack(// Outputs
+	      .write	(),
+	      .command	(),
+	      .size	(),
+	      .options	(),
+	      .dstaddr	(dstaddr[AW-1:0]),
+	      .srcaddr	(),
+	      .data	(),
+	      // Inputs
+	      .packet	(packet[UW-1:0]));
+
+endmodule
