8c713009d914d2255546473e6614421548d9f35c
Andreas Olofsson <andreas@zeroasic.com>
2023-04-11 21:42:16 -0400

    Updating register interface to - Need to add a testbench...

--- umi/rtl/umi_regif.v
+++ umi/rtl/umi_regif.v
@@ -5,28 +5,29 @@
  *
  * Documentation:
  *
- * - oob host responsible for ordering
- * - one read request every two clock cycles
+ * The module translates a UMI request into a simple register interface.
+ * Read data is returned as UMI response packets. Reads requests can occur
+ * at a maximum rate of one transaction every two cycles. 
+ * 
+ * Only read/writes <= DW is supported.  
  *
  ******************************************************************************/
 module umi_regif
   #(parameter TARGET = "DEFAULT", // compile target
     parameter AW     = 64,        // address width
-    parameter UW     = 256,       // packet width
-    parameter IDW    = 16,        // ID width
-    // derived
-    parameter DW     = AW         // data width
+    parameter DW     = AW,        // data width
+    parameter UW     = 256        // packet width
     )
    (// clk, reset
     input 	      clk, //clk
     input 	      nreset, //async active low reset
-    // OOB access
-    input 	      oob_in_valid,
-    input [UW-1:0]    oob_in_packet,
-    output reg 	      oob_in_ready,
-    output reg 	      oob_out_valid,
-    output [UW-1:0]   oob_out_packet,
-    input 	      oob_out_ready,
+    // UMI access
+    input 	      udev_req_valid,
+    input [UW-1:0]    udev_req_packet,
+    output reg 	      udev_req_ready,
+    output reg 	      udev_resp_valid,
+    output [UW-1:0]   udev_resp_packet,
+    input 	      udev_resp_ready,
     // Read/Write register interface
     output [AW-1:0]   reg_addr, // memory address
     output 	      reg_write, // register write
@@ -34,14 +35,14 @@ module umi_regif
     output [7:0]      reg_cmd, // command (eg. atomics)
     output [3:0]      reg_size, // size (byte, etc)
     output [4*DW-1:0] reg_wrdata, // data to write
-    input [DW-1:0]    reg_rddata  // readback data
+    input [DW-1:0]    reg_rddata // readback data
     );
 
 `include "umi_messages.vh"
-
+   
    wire [AW-1:0]      reg_srcaddr;
    wire [19:0] 	      reg_options;
-
+   
    //########################
    // UMI INPUT
    //########################
@@ -57,19 +58,20 @@ module umi_regif
 	      .srcaddr	(reg_srcaddr[AW-1:0]),
 	      .data	(reg_wrdata[4*AW-1:0]),
 	      // Inputs
-	      .packet	(oob_in_packet[UW-1:0]));
-
-   assign reg_read  = ~write & oob_in_valid;
-   assign reg_write = write & oob_in_valid;
-
+	      .packet	(udev_req_packet[UW-1:0]));
+   
+   assign reg_read  = ~write & udev_req_valid;
+   assign reg_write =  write & udev_req_valid;
+      
+   // single cycle stall on every ready
    always @ (posedge clk or negedge nreset)
      if(!nreset)
-       oob_in_ready <= 1'b1;
-     else if (oob_out_valid & oob_out_ready)
-       oob_in_ready <= 1'b1;
-     else if (reg_read)
-       oob_in_ready <= 1'b0;
-
+       udev_req_ready <= 1'b0;
+     else if (udev_req_valid & udev_resp_ready)
+       udev_req_ready <= ~udev_req_ready;
+     else
+       udev_req_ready <= 1'b0;
+    
    //############################
    //# UMI OUTPUT
    //############################
@@ -77,20 +79,19 @@ module umi_regif
    //1. Set on incoming valid read
    //2. Keep high as long as incoming read is set
    //3. If no incoming read and output is ready, clear
-   //4. Pne read every two clock cycles
 
    always @ (posedge clk or negedge nreset)
      if(!nreset)
-       oob_out_valid <= 1'b0;
+       udev_resp_valid <= 1'b0;
      else if (reg_read)
-       oob_out_valid <= 1'b1;
-     else if (oob_out_valid & oob_out_ready)
-       oob_out_valid <= 1'b0;
-
+       udev_resp_valid <= 1'b1;
+     else if (udev_resp_valid & udev_resp_ready)
+       udev_resp_valid <= 1'b0;
+   
    umi_pack #(.UW(UW),
 	      .AW(AW))
    umi_pack(// Outputs
-	    .packet	(oob_out_packet[UW-1:0]),
+	    .packet	(udev_resp_packet[UW-1:0]),
 	    // Inputs
 	    .write	(1'b1),
 	    .command    (UMI_WRITE_RESPONSE),//returns write response
