809c3a5e6101dcfac57b70e17126b2201a655d38
aolofsson <andreas@zeroasic.com>
2023-04-24 08:51:54 -0400

    Simplify mux   - Assume one hot inputs   - No need for arbiter

--- umi/rtl/umi_mux.v
+++ umi/rtl/umi_mux.v
@@ -1,70 +1,42 @@
 /*******************************************************************************
- * Function:  UMI Mux
+ * Function:  UMI Mux (one-hot)
  * Author:    Andreas Olofsson
  * License:
  *
  * Documentation:
  *
  * - Selects between N inputs
- * - Input 0 has highest priority
+ * - Assumes one-hot selects
  *
  ******************************************************************************/
 module umi_mux
   #(parameter UW  = 256, // UMI transaction width
     parameter N   = 4    // number of inputs
     )
-   (// controls
-    input 		clk,
-    input 		nreset,
-    input [1:0] 	mode, // arbiter mode (0=fixed)
-    input [N-1:0] 	mask, // 1=disables input request
-    // Incoming UMI
-    input [N-1:0] 	umi_in_valid,
-    input [N*UW-1:0] 	umi_in_packet,
-    output [N-1:0] 	umi_in_ready,
+   (// Incoming UMI
+    input [N-1:0]    umi_in_valid,
+    input [N*UW-1:0] umi_in_packet,
+    output [N-1:0]   umi_in_ready,
     // Outgoing UMI
-    output 		umi_out_valid,
-    output reg [UW-1:0] umi_out_packet
+    output 	     umi_out_valid,
+    input 	     umi_out_ready,
+    output [UW-1:0]  umi_out_packet
     );
 
-   // local wires
-   wire [N-1:0]       grants;
+   // valid output
+   assign umi_out_valid = |umi_in_valid[N-1:0];
 
-   /*AUTOWIRE*/
-
-   //##############################
-   // Valid Selection
-   //##############################
-
-   umi_arbiter #(.N(N))
-   umi_arbiter(// outputs
-	       .grants		(grants[N-1:0]),
-	       // Inputs
-	       .clk		(clk),
-	       .nreset		(nreset),
-	       .mode		(mode[1:0]),
-	       .mask		(mask[N-1:0]),
-	       .requests	(umi_in_valid[N-1:0]));
-
-   assign umi_out_valid = |grants[N-1:0];
-
+   // ready pusback
    assign umi_in_ready[N-1:0] = ~umi_in_valid[N-1:0] |
-				(umi_in_valid[N-1:0] & grants[N-1:0]);
+				(umi_in_valid[N-1:0] & umi_out_ready);
 
-   //##############################
-   // Packet Mux
-   //##############################
+   // packet mux
+   la_vmux #(.N(3),
+	     .W(UW))
+   la_vmux(.out (umi_out_packet[UW-1:0]),
+	   .sel (umi_in_valid[UW-1:0]),
+	   .in  (umi_in_packet[N*UW-1:0]));
 
-   // Select outgoing packet
-   integer 	      i;
-   always @*
-     begin
-	umi_out_packet[UW-1:0] = 'b0;
-	for(i=0;i<N;i=i+1)
-	  begin
-	     umi_out_packet[UW-1:0] = umi_out_packet |
-	                              {UW{grants[i]}} & umi_in_packet[i*UW+:UW];
-	  end
-     end
+   //TODO: add checker for one hot!
 
 endmodule
