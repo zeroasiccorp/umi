52cb90d1ee54c8a199d0d73705fc8ea19f571340
volkamir <amir@zeroasic.com>
2023-05-30 02:16:51 -0400

    updating two unused blocks for completion

--- umi/rtl/umi_pipeline.v
+++ umi/rtl/umi_pipeline.v
@@ -14,17 +14,25 @@
  *
  ******************************************************************************/
 module umi_pipeline
-  #(parameter UW  = 256
+  #(parameter CW  = 32,
+    parameter AW  = 64,
+    parameter UW  = 256
     )
    (// clock, reset
     input 		clk,
     input 		nreset,
     // Incoming UMI request
     input 		umi_in_valid,
-    input [UW-1:0] 	umi_in_packet,
+    input [CW-1:0] 	umi_in_cmd,
+    input [AW-1:0] 	umi_in_dst_addr,
+    input [AW-1:0] 	umi_in_src_addr,
+    input [UW-1:0] 	umi_in_payload,
     // Outgoing UMI response
     output reg 		umi_out_valid,
-    output reg [UW-1:0] umi_out_packet,
+    output reg [CW-1:0] umi_out_cmd,
+    output reg [AW-1:0] umi_out_dst_addr,
+    output reg [AW-1:0] umi_out_src_addr,
+    output reg [UW-1:0] umi_out_payload,
     input 		umi_out_ready
     );
 
@@ -38,6 +46,11 @@ module umi_pipeline
    // packet
    always @ (posedge clk)
      if (umi_out_ready & umi_in_valid)
-       umi_out_packet[UW-1:0] <= umi_in_packet[UW-1:0];
+       begin
+          umi_out_cmd[CW-1:0]      <= umi_in_cmd[CW-1:0];
+          umi_out_dst_addr[AW-1:0] <= umi_in_dst_addr[AW-1:0];
+          umi_out_src_addr[AW-1:0] <= umi_in_src_addr[AW-1:0];
+          umi_out_payload[UW-1:0]  <= umi_in_payload[UW-1:0];
+       end
 
 endmodule
