96e0adec2c1f3ecd99496786d667afd4c026c4e8
volkamir <amir@zeroasic.com>
2023-05-30 02:16:51 -0400

    updating two unused blocks for completion

--- umi/rtl/umi_combiner.v
+++ umi/rtl/umi_combiner.v
@@ -15,21 +15,31 @@
 module umi_combiner
   #(// standard parameters
     parameter AW   = 64,
+    parameter CW   = 32,
     parameter UW   = 256)
    (// controls
     input 	    clk,
     input 	    nreset,
     // Input (0), Higher Priority
     input 	    umi_resp_in_valid,
-    input [UW-1:0]  umi_resp_in_packet,
+    input [CW-1:0]  umi_resp_in_cmd,
+    input [AW-1:0]  umi_resp_in_dst_addr,
+    input [AW-1:0]  umi_resp_in_src_addr,
+    input [UW-1:0]  umi_resp_in_payload,
     output 	    umi_resp_in_ready,
     // Input (1)
     input 	    umi_req_in_valid,
-    input [UW-1:0]  umi_req_in_packet,
+    input [CW-1:0]  umi_req_in_cmd,
+    input [AW-1:0]  umi_req_in_dst_addr,
+    input [AW-1:0]  umi_req_in_src_addr,
+    input [UW-1:0]  umi_req_in_payload,
     output 	    umi_req_in_ready,
     // Output
     output 	    umi_out_valid,
-    output [UW-1:0] umi_out_packet,
+    output [CW-1:0] umi_out_cmd,
+    output [AW-1:0] umi_out_dst_addr,
+    output [AW-1:0] umi_out_src_addr,
+    output [UW-1:0] umi_out_payload,
     input 	    umi_out_ready
     );
 
@@ -41,14 +51,23 @@ module umi_combiner
    umi_mux (// Outputs
 	    .umi_in_ready	({umi_req_ready,umi_resp_ready}),
 	    .umi_out_valid	(umi_out_valid),
-	    .umi_out_packet	(umi_out_packet[UW-1:0]),
+            .umi_out_cmd        (umi_out_cmd[CW-1:0]),
+            .umi_out_dst_addr   (umi_out_dst_addr[AW-1:0]),
+            .umi_out_src_addr   (umi_out_src_addr[AW-1:0]),
+            .umi_out_payload    (umi_out_payload[UW-1:0]),
 	    // Inputs
 	    .clk		(clk),
 	    .nreset		(nreset),
 	    .mode		(2'b00),
 	    .mask		(2'b00),
 	    .umi_in_valid	({umi_req_in_valid, umi_resp_in_valid}),
-	    .umi_in_packet	({umi_req_in_packet, umi_resp_in_packet}));
+	    .umi_in_cmd 	({umi_req_in_cmd, umi_resp_in_cmd}),
+	    .umi_in_dst_addr	({umi_req_in_dst_addr, umi_resp_in_dst_addr}),
+	    .umi_in_src_addr	({umi_req_in_src_addr, umi_resp_in_src_addr}),
+	    .umi_in_payload	({umi_req_in_payload, umi_resp_in_payload}),
+            /*AUTOINST*/
+            // Inputs
+            .umi_out_ready      (umi_out_ready));
 
    // Flow through pushback
    assign umi_resp_in_ready = umi_out_ready & umi_resp_ready;
