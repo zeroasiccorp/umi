38b6ab7da57b0ff27346035f5e18550f59ebb863
volkamir <amir@zeroasic.com>
2023-06-06 22:50:09 -0700

    update regif to support responce on read and write

--- umi/rtl/umi_write.v
+++ umi/rtl/umi_write.v
@@ -7,36 +7,53 @@
  *
  *
  ******************************************************************************/
-module umi_write
-  (
-   input [7:0] command, // Packet Command
-   output      write    // write indicator
-   );
+module umi_write #(parameter CW = 32)
+   (
+    input [7:0] command,     // Packet Command
+    output      write,       // write indicator
+    output      write_posted // write indicator
+    );
 
-   umi_decode
-     umi_decode(// Outputs
-		.cmd_write		(write),
-		.cmd_request		(),
-		.cmd_response		(),
-		.cmd_invalid		(),
-		.cmd_read       	(),
-		.cmd_write_posted	(),
-		.cmd_write_signal	(),
-		.cmd_write_ack		(),
-		.cmd_write_stream	(),
-		.cmd_write_response	(),
-		.cmd_write_multicast	(),
-		.cmd_atomic		(),
-		.cmd_atomic_swap	(),
-		.cmd_atomic_add		(),
-		.cmd_atomic_and		(),
-		.cmd_atomic_or		(),
-		.cmd_atomic_xor		(),
-		.cmd_atomic_min		(),
-		.cmd_atomic_max		(),
-		.cmd_atomic_minu	(),
-		.cmd_atomic_maxu	(),
-		// Inputs
-		.command		(command[7:0]));
+   /*umi_decode AUTO_TEMPLATE(
+    .cmd_write        (write[]),
+    .cmd_write_posted (write_posted[]),
+    .cmd_.*    (),
+    .command   ({24'h00_0000,command[7:0]}),
+    );*/
+
+   // Command decode
+   umi_decode #(.CW(CW))
+   umi_decode(/*AUTOINST*/
+              // Outputs
+              .cmd_invalid      (),                      // Templated
+              .cmd_request      (),                      // Templated
+              .cmd_response     (),                      // Templated
+              .cmd_read         (),                      // Templated
+              .cmd_write        (write),                 // Templated
+              .cmd_write_posted (write_posted),          // Templated
+              .cmd_rdma         (),                      // Templated
+              .cmd_atomic       (),                      // Templated
+              .cmd_user0        (),                      // Templated
+              .cmd_future0      (),                      // Templated
+              .cmd_error        (),                      // Templated
+              .cmd_link         (),                      // Templated
+              .cmd_read_resp    (),                      // Templated
+              .cmd_write_resp   (),                      // Templated
+              .cmd_user0_resp   (),                      // Templated
+              .cmd_user1_resp   (),                      // Templated
+              .cmd_future0_resp (),                      // Templated
+              .cmd_future1_resp (),                      // Templated
+              .cmd_link_resp    (),                      // Templated
+              .cmd_atomic_add   (),                      // Templated
+              .cmd_atomic_and   (),                      // Templated
+              .cmd_atomic_or    (),                      // Templated
+              .cmd_atomic_xor   (),                      // Templated
+              .cmd_atomic_max   (),                      // Templated
+              .cmd_atomic_min   (),                      // Templated
+              .cmd_atomic_maxu  (),                      // Templated
+              .cmd_atomic_minu  (),                      // Templated
+              .cmd_atomic_swap  (),                      // Templated
+              // Inputs
+              .command          ({24'h00_0000,command[7:0]})); // Templated
 
 endmodule
