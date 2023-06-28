2683644638e4c7a4489d13667e0ee0c4c3778839
volkamir <amir@zeroasic.com>
2023-06-28 12:58:11 -0700

    build script

--- umi/testbench/build_local.sh
+++ umi/testbench/build_local.sh
@@ -1,13 +1,13 @@
 \rm -f a.out
 
-iverilog tb_umi_regif.v \
+iverilog tb_umi_arbiter.v \
 	 -y . \
 	 -y ../rtl \
 	 -I . \
 	 -I ../rtl \
-	 -y ../../submoduels/lambdalib/stdlib/rtl \
+	 -y ../../submodules/lambdalib/stdlib/rtl \
 	 -y ../../submodules/lambdalib/ramlib/rtl
 
 ./a.out
-//./a.out +MEMHFILE=./hello.memh
-//./a.out +MEMHFILE=./buffer.memh
+#./a.out +MEMHFILE=./hello.memh
+#./a.out +MEMHFILE=./buffer.memh
