ab5549afb04bb66b100107d630605a879f3cefaf
volkamir <amir@zeroasic.com>
2023-06-30 10:10:26 -0700

    change name from width to flex

--- umi/testbench/dut_umi_fifo_flex.v
+++ umi/testbench/dut_umi_fifo_flex.v
@@ -9,7 +9,7 @@
  *
  *****************************************************************************/
 
-module dut_umi_fifo_width
+module dut_umi_fifo_flex
   #(parameter TARGET   = "DEFAULT", // synthesis target
     parameter MONITOR  = 1,         // turn on umi monitor
     parameter NUMI     = 1,         // number of umi interfaces
@@ -18,10 +18,9 @@ module dut_umi_fifo_width
     parameter NSTATUS  = 1,         // number of status pins
     // for development
     parameter CW       = 32,       // umi width
-    parameter IAW       = 64,       // umi width
-    parameter IDW       = 512,       // umi width
-    parameter OAW       = 64,       // umi width
-    parameter ODW       = 512,       // umi width
+    parameter AW       = 64,       // umi width
+    parameter IDW      = 512,       // umi width
+    parameter ODW      = 512,       // umi width
     parameter DEPTH    = 4          // fifo depth
     )
    (// generic control interface
@@ -37,16 +36,16 @@ module dut_umi_fifo_width
     input [NUMI-1:0] 	 umi_in_nreset,
     input [NUMI-1:0] 	 umi_in_valid,
     input [NUMI*CW-1:0]  umi_in_cmd,
-    input [NUMI*IAW-1:0]  umi_in_dstaddr,
-    input [NUMI*IAW-1:0]  umi_in_srcaddr,
+    input [NUMI*AW-1:0]  umi_in_dstaddr,
+    input [NUMI*AW-1:0]  umi_in_srcaddr,
     input [NUMI*IDW-1:0]  umi_in_data,
     output [NUMI-1:0] 	 umi_in_ready,
     input [NUMI-1:0] 	 umi_out_clk,
     input [NUMI-1:0] 	 umi_out_nreset,
     output [NUMI-1:0] 	 umi_out_valid,
     output [NUMI*CW-1:0] umi_out_cmd,
-    output [NUMI*OAW-1:0] umi_out_dstaddr,
-    output [NUMI*OAW-1:0] umi_out_srcaddr,
+    output [NUMI*AW-1:0] umi_out_dstaddr,
+    output [NUMI*AW-1:0] umi_out_srcaddr,
     output [NUMI*ODW-1:0] umi_out_data,
     input [NUMI-1:0] 	 umi_out_ready
     );
@@ -63,41 +62,40 @@ module dut_umi_fifo_width
    //# HOST
    //#################################
 
-   /* umi_fifo_width AUTO_TEMPLATE(
+   /* umi_fifo_flex AUTO_TEMPLATE(
     .chaosmode (1'b0),
     );*/
-   umi_fifo_width  #(.CW(CW),
-	             .IAW(IAW),
-	             .IDW(IDW),
-	             .OAW(OAW),
-	             .ODW(ODW),
-	             .DEPTH(DEPTH),
-	             .TARGET(TARGET))
-   umi_fifo_width (.bypass			(1'b0),
-	           .vdd			(1'b1),
-	           .vss			(1'b0),
+   umi_fifo_flex  #(.CW(CW),
+	            .AW(AW),
+	            .IDW(IDW),
+	            .ODW(ODW),
+	            .DEPTH(DEPTH),
+	            .TARGET(TARGET))
+   umi_fifo_flex (.bypass		(1'b0),
+	          .vdd			(1'b1),
+	          .vss			(1'b0),
 	           /*AUTOINST*/
-                   // Outputs
-                   .fifo_full           (fifo_full),
-                   .fifo_empty          (fifo_empty),
-                   .umi_in_ready        (umi_in_ready),
-                   .umi_out_valid       (umi_out_valid),
-                   .umi_out_cmd         (umi_out_cmd[CW-1:0]),
-                   .umi_out_dstaddr     (umi_out_dstaddr[OAW-1:0]),
-                   .umi_out_srcaddr     (umi_out_srcaddr[OAW-1:0]),
-                   .umi_out_data        (umi_out_data[ODW-1:0]),
-                   // Inputs
-                   .chaosmode           (1'b0),                  // Templated
-                   .umi_in_clk          (umi_in_clk),
-                   .umi_in_nreset       (umi_in_nreset),
-                   .umi_in_valid        (umi_in_valid),
-                   .umi_in_cmd          (umi_in_cmd[CW-1:0]),
-                   .umi_in_dstaddr      (umi_in_dstaddr[IAW-1:0]),
-                   .umi_in_srcaddr      (umi_in_srcaddr[IAW-1:0]),
-                   .umi_in_data         (umi_in_data[IDW-1:0]),
-                   .umi_out_clk         (umi_out_clk),
-                   .umi_out_nreset      (umi_out_nreset),
-                   .umi_out_ready       (umi_out_ready));
+                  // Outputs
+                  .fifo_full            (fifo_full),
+                  .fifo_empty           (fifo_empty),
+                  .umi_in_ready         (umi_in_ready),
+                  .umi_out_valid        (umi_out_valid),
+                  .umi_out_cmd          (umi_out_cmd[CW-1:0]),
+                  .umi_out_dstaddr      (umi_out_dstaddr[AW-1:0]),
+                  .umi_out_srcaddr      (umi_out_srcaddr[AW-1:0]),
+                  .umi_out_data         (umi_out_data[ODW-1:0]),
+                  // Inputs
+                  .chaosmode            (1'b0),                  // Templated
+                  .umi_in_clk           (umi_in_clk),
+                  .umi_in_nreset        (umi_in_nreset),
+                  .umi_in_valid         (umi_in_valid),
+                  .umi_in_cmd           (umi_in_cmd[CW-1:0]),
+                  .umi_in_dstaddr       (umi_in_dstaddr[AW-1:0]),
+                  .umi_in_srcaddr       (umi_in_srcaddr[AW-1:0]),
+                  .umi_in_data          (umi_in_data[IDW-1:0]),
+                  .umi_out_clk          (umi_out_clk),
+                  .umi_out_nreset       (umi_out_nreset),
+                  .umi_out_ready        (umi_out_ready));
 
 endmodule // testbench
 // Local Variables:
