ddf0c7ac312fde837df57746f67779dbb2584d55
volkamir <amir@zeroasic.com>
2023-06-30 10:48:05 -0700

    add support for bypass mode

--- umi/testbench/dut_umi_fifo_flex.v
+++ umi/testbench/dut_umi_fifo_flex.v
@@ -10,48 +10,49 @@
  *****************************************************************************/
 
 module dut_umi_fifo_flex
-  #(parameter TARGET   = "DEFAULT", // synthesis target
-    parameter MONITOR  = 1,         // turn on umi monitor
-    parameter NUMI     = 1,         // number of umi interfaces
-    parameter NCLK     = 1,         // number of clk pins
-    parameter NCTRL    = 1,         // number of ctrl pins
-    parameter NSTATUS  = 1,         // number of status pins
+  #(parameter TARGET = "DEFAULT", // synthesis target
+    parameter BYPASS = 0,
+    parameter MONITOR = 1,        // turn on umi monitor
+    parameter NUMI = 1,           // number of umi interfaces
+    parameter NCLK = 1,           // number of clk pins
+    parameter NCTRL = 1,          // number of ctrl pins
+    parameter NSTATUS = 1,        // number of status pins
     // for development
-    parameter CW       = 32,       // umi width
-    parameter AW       = 64,       // umi width
-    parameter IDW      = 512,       // umi width
-    parameter ODW      = 512,       // umi width
-    parameter DEPTH    = 4          // fifo depth
+    parameter CW = 32,            // umi width
+    parameter AW = 64,            // umi width
+    parameter IDW = 512,          // umi width
+    parameter ODW = 512,          // umi width
+    parameter DEPTH = 4           // fifo depth
     )
    (// generic control interface
-    input 		 nreset, // common async active low reset
-    input [NCLK-1:0] 	 clk, //  generic set of clocks
-    input 		 go, // go dut, go!
-    output 		 error, //dut  error
-    output 		 done, // dut done
-    input [NCTRL-1:0] 	 ctrl, // generic control vector (optional)
-    output [NSTATUS-1:0] status, // generic status vector (optional)
+    input                 nreset, // common async active low reset
+    input [NCLK-1:0]      clk,    //  generic set of clocks
+    input                 go,     // go dut, go!
+    output                error,  //dut  error
+    output                done,   // dut done
+    input [NCTRL-1:0]     ctrl,   // generic control vector (optional)
+    output [NSTATUS-1:0]  status, // generic status vector (optional)
+    input                 bypass,
     // umi interfaces
-    input [NUMI-1:0] 	 umi_in_clk,
-    input [NUMI-1:0] 	 umi_in_nreset,
-    input [NUMI-1:0] 	 umi_in_valid,
-    input [NUMI*CW-1:0]  umi_in_cmd,
-    input [NUMI*AW-1:0]  umi_in_dstaddr,
-    input [NUMI*AW-1:0]  umi_in_srcaddr,
+    input [NUMI-1:0]      umi_in_clk,
+    input [NUMI-1:0]      umi_in_nreset,
+    input [NUMI-1:0]      umi_in_valid,
+    input [NUMI*CW-1:0]   umi_in_cmd,
+    input [NUMI*AW-1:0]   umi_in_dstaddr,
+    input [NUMI*AW-1:0]   umi_in_srcaddr,
     input [NUMI*IDW-1:0]  umi_in_data,
-    output [NUMI-1:0] 	 umi_in_ready,
-    input [NUMI-1:0] 	 umi_out_clk,
-    input [NUMI-1:0] 	 umi_out_nreset,
-    output [NUMI-1:0] 	 umi_out_valid,
-    output [NUMI*CW-1:0] umi_out_cmd,
-    output [NUMI*AW-1:0] umi_out_dstaddr,
-    output [NUMI*AW-1:0] umi_out_srcaddr,
+    output [NUMI-1:0]     umi_in_ready,
+    input [NUMI-1:0]      umi_out_clk,
+    input [NUMI-1:0]      umi_out_nreset,
+    output [NUMI-1:0]     umi_out_valid,
+    output [NUMI*CW-1:0]  umi_out_cmd,
+    output [NUMI*AW-1:0]  umi_out_dstaddr,
+    output [NUMI*AW-1:0]  umi_out_srcaddr,
     output [NUMI*ODW-1:0] umi_out_data,
-    input [NUMI-1:0] 	 umi_out_ready
+    input [NUMI-1:0]      umi_out_ready
     );
 
    // Local wires
-   reg 		slowclk;
    wire 	fifo_empty;
    wire 	fifo_full;
 
@@ -70,8 +71,9 @@ module dut_umi_fifo_flex
 	            .IDW(IDW),
 	            .ODW(ODW),
 	            .DEPTH(DEPTH),
-	            .TARGET(TARGET))
-   umi_fifo_flex (.bypass		(1'b0),
+	            .TARGET(TARGET),
+                    .BYPASS(BYPASS))
+   umi_fifo_flex (.bypass		(bypass),
 	          .vdd			(1'b1),
 	          .vss			(1'b0),
 	           /*AUTOINST*/
