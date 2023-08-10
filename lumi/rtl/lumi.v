80c9e872e7c96eb896df4515d37ef46eb77df661
volkamir <amir@zeroasic.com>
2023-08-10 12:33:12 -0700

    align integration

--- lumi/rtl/lumi.v
+++ lumi/rtl/lumi.v
@@ -29,7 +29,7 @@ module lumi
     parameter CRDTFIFOD = 64      // Fifo size need to account for 64B over 2B link
     )
    (// host/device selector
-    input            devicemode,     // 1=device, 0=host
+    input            devicemode,      // 1=device, 0=host
     // UMI host port
     output           uhost_req_valid,
     output [CW-1:0]  uhost_req_cmd,
@@ -57,26 +57,26 @@ module lumi
     output [DW-1:0]  udev_resp_data,
     input            udev_resp_ready,
     // LinkHost sideband interface - register access
-    input            sb_in_valid,    // host-request, device-response
+    input            sb_in_valid,     // host-request, device-response
     input [CW-1:0]   sb_in_cmd,
     input [AW-1:0]   sb_in_dstaddr,
     input [AW-1:0]   sb_in_srcaddr,
     input [RW-1:0]   sb_in_data,
     output           sb_in_ready,
-    output           sb_out_valid,   // device-request, host-response
+    output           sb_out_valid,    // device-request, host-response
     output [CW-1:0]  sb_out_cmd,
     output [AW-1:0]  sb_out_dstaddr,
     output [AW-1:0]  sb_out_srcaddr,
     output [RW-1:0]  sb_out_data,
     input            sb_out_ready,
     // phy sideband interface - paththrough based on address
-    input            phy_in_valid,   // host-response, device-request
+    input            phy_in_valid,    // host-response, device-request
     input [CW-1:0]   phy_in_cmd,
     input [AW-1:0]   phy_in_dstaddr,
     input [AW-1:0]   phy_in_srcaddr,
     input [RW-1:0]   phy_in_data,
     output           phy_in_ready,
-    output           phy_out_valid,  // host-request, device-response
+    output           phy_out_valid,   // host-request, device-response
     output [CW-1:0]  phy_out_cmd,
     output [AW-1:0]  phy_out_dstaddr,
     output [AW-1:0]  phy_out_srcaddr,
@@ -92,38 +92,22 @@ module lumi
     // phy control interface
     input            phy_linkactive,
     // Host control interface
-    input            nreset,         // host driven reset
-    input            clk,            // host driven clock
+    input            nreset,          // host driven reset
+    input            clk,             // host driven clock
     input            devicready,
     output           host_linkactive, // link is locked/ready
     // supplies
-    input            vss,            // common ground
-    input            vdd,            // core supply
-    input            vddio,          // io voltage
+    input            vss,             // common ground
+    input            vdd,             // core supply
+    input            vddio            // io voltage
     /*AUTOINPUT*/
-    // Beginning of automatic inputs (from unused autoinst inputs)
-    input               cb2serial_ready,
-    input [CW-1:0]      serial2cb_cmd,
-    input [DW-1:0]      serial2cb_data,
-    input [AW-1:0]      serial2cb_dstaddr,
-    input [AW-1:0]      serial2cb_srcaddr,
-    input               serial2cb_valid,
-    // End of automatics
     /*AUTOOUTPUT*/
-    // Beginning of automatic outputs (from unused autoinst outputs)
-    output [CW-1:0]     cb2serial_cmd,
-    output [DW-1:0]     cb2serial_data,
-    output [AW-1:0]     cb2serial_dstaddr,
-    output [AW-1:0]     cb2serial_srcaddr,
-    output              cb2serial_valid,
-    output              serial2cb_ready
-    // End of automatics
     );
 
    /*AUTOWIRE*/
    // Beginning of automatic wires (for undeclared instantiated-module outputs)
    wire [CW-1:0]        cb2regs_cmd;
-   wire [DW-1:0]        cb2regs_data;
+   wire [RW-1:0]        cb2regs_data;
    wire [AW-1:0]        cb2regs_dstaddr;
    wire                 cb2regs_ready;
    wire [AW-1:0]        cb2regs_srcaddr;
@@ -140,7 +124,7 @@ module lumi
    wire [15:0]          loc_crdt_req;
    wire [15:0]          loc_crdt_resp;
    wire [CW-1:0]        regs2cb_cmd;
-   wire [DW-1:0]        regs2cb_data;
+   wire [RW-1:0]        regs2cb_data;
    wire [AW-1:0]        regs2cb_dstaddr;
    wire                 regs2cb_ready;
    wire [AW-1:0]        regs2cb_srcaddr;
@@ -163,14 +147,13 @@ module lumi
     */
 
    lumi_regs #(.TARGET(TARGET),
-                .GRPOFFSET(GRPOFFSET),
-                .GRPAW(GRPAW),
-		.GRPID(GRPID),
-                .DW(DW),
-                .RW(RW),
-                .CW(CW),
-		.AW(AW),
-                .CRDTFIFOD(CRDTFIFOD))
+               .GRPOFFSET(GRPOFFSET),
+               .GRPAW(GRPAW),
+	       .GRPID(GRPID),
+               .RW(RW),
+               .CW(CW),
+	       .AW(AW),
+               .CRDTFIFOD(CRDTFIFOD))
    lumi_regs(/*AUTOINST*/
              // Outputs
              .nreset            (nreset),
@@ -180,7 +163,7 @@ module lumi
              .udev_resp_cmd     (regs2cb_cmd[CW-1:0]),   // Templated
              .udev_resp_dstaddr (regs2cb_dstaddr[AW-1:0]), // Templated
              .udev_resp_srcaddr (regs2cb_srcaddr[AW-1:0]), // Templated
-             .udev_resp_data    (regs2cb_data[DW-1:0]),  // Templated
+             .udev_resp_data    (regs2cb_data[RW-1:0]),  // Templated
              .host_linkactive   (host_linkactive),
              .csr_arbmode       (),                      // Templated
              .csr_txen          (csr_txen),
@@ -198,7 +181,7 @@ module lumi
              .udev_req_cmd      (cb2regs_cmd[CW-1:0]),   // Templated
              .udev_req_dstaddr  (cb2regs_dstaddr[AW-1:0]), // Templated
              .udev_req_srcaddr  (cb2regs_srcaddr[AW-1:0]), // Templated
-             .udev_req_data     (cb2regs_data[DW-1:0]),  // Templated
+             .udev_req_data     (cb2regs_data[RW-1:0]),  // Templated
              .udev_resp_ready   (regs2cb_ready),         // Templated
              .phy_linkactive    (phy_linkactive),
              .csr_txcrdt_status (csr_txcrdt_status[31:0]));
@@ -210,20 +193,18 @@ module lumi
    /*lumi_crossbar  AUTO_TEMPLATE (
     .regs_out_\(.*\)    (cb2regs_\1[]),
     .regs_in_\(.*\)     (regs2cb_\1[]),
-    .serial_out_\(.*\)  (cb2serial_\1[]),
-    .serial_in_\(.*\)   (serial2cb_\1[]),
     .core_out_\(.*\)    (sb_out_\1[]),
     .core_in_\(.*\)     (sb_in_\1[]),
     )
     */
 
    lumi_crossbar #(.AW(AW),
-                    .CW(CW),
-                    .DW(DW),
-                    .IDOFFSET(IDOFFSET),
-                    .GRPOFFSET(GRPOFFSET),
-                    .GRPAW(GRPAW),
-                    .GRPID(GRPID))
+                   .CW(CW),
+                   .DW(DW),
+                   .IDOFFSET(IDOFFSET),
+                   .GRPOFFSET(GRPOFFSET),
+                   .GRPAW(GRPAW),
+                   .GRPID(GRPID))
    lumi_crossbar(/*AUTOINST*/
                  // Outputs
                  .core_in_ready         (sb_in_ready),           // Templated
@@ -231,19 +212,19 @@ module lumi
                  .core_out_cmd          (sb_out_cmd[CW-1:0]),    // Templated
                  .core_out_dstaddr      (sb_out_dstaddr[AW-1:0]), // Templated
                  .core_out_srcaddr      (sb_out_srcaddr[AW-1:0]), // Templated
-                 .core_out_data         (sb_out_data[DW-1:0]),   // Templated
-                 .serial_in_ready       (serial2cb_ready),       // Templated
-                 .serial_out_valid      (cb2serial_valid),       // Templated
-                 .serial_out_cmd        (cb2serial_cmd[CW-1:0]), // Templated
-                 .serial_out_dstaddr    (cb2serial_dstaddr[AW-1:0]), // Templated
-                 .serial_out_srcaddr    (cb2serial_srcaddr[AW-1:0]), // Templated
-                 .serial_out_data       (cb2serial_data[DW-1:0]), // Templated
+                 .core_out_data         (sb_out_data[RW-1:0]),   // Templated
+                 .phy_in_ready          (phy_in_ready),
+                 .phy_out_valid         (phy_out_valid),
+                 .phy_out_cmd           (phy_out_cmd[CW-1:0]),
+                 .phy_out_dstaddr       (phy_out_dstaddr[AW-1:0]),
+                 .phy_out_srcaddr       (phy_out_srcaddr[AW-1:0]),
+                 .phy_out_data          (phy_out_data[RW-1:0]),
                  .regs_in_ready         (regs2cb_ready),         // Templated
                  .regs_out_valid        (cb2regs_valid),         // Templated
                  .regs_out_cmd          (cb2regs_cmd[CW-1:0]),   // Templated
                  .regs_out_dstaddr      (cb2regs_dstaddr[AW-1:0]), // Templated
                  .regs_out_srcaddr      (cb2regs_srcaddr[AW-1:0]), // Templated
-                 .regs_out_data         (cb2regs_data[DW-1:0]),  // Templated
+                 .regs_out_data         (cb2regs_data[RW-1:0]),  // Templated
                  // Inputs
                  .nreset                (nreset),
                  .clk                   (clk),
@@ -252,19 +233,19 @@ module lumi
                  .core_in_cmd           (sb_in_cmd[CW-1:0]),     // Templated
                  .core_in_dstaddr       (sb_in_dstaddr[AW-1:0]), // Templated
                  .core_in_srcaddr       (sb_in_srcaddr[AW-1:0]), // Templated
-                 .core_in_data          (sb_in_data[DW-1:0]),    // Templated
+                 .core_in_data          (sb_in_data[RW-1:0]),    // Templated
                  .core_out_ready        (sb_out_ready),          // Templated
-                 .serial_in_valid       (serial2cb_valid),       // Templated
-                 .serial_in_cmd         (serial2cb_cmd[CW-1:0]), // Templated
-                 .serial_in_dstaddr     (serial2cb_dstaddr[AW-1:0]), // Templated
-                 .serial_in_srcaddr     (serial2cb_srcaddr[AW-1:0]), // Templated
-                 .serial_in_data        (serial2cb_data[DW-1:0]), // Templated
-                 .serial_out_ready      (cb2serial_ready),       // Templated
+                 .phy_in_valid          (phy_in_valid),
+                 .phy_in_cmd            (phy_in_cmd[CW-1:0]),
+                 .phy_in_dstaddr        (phy_in_dstaddr[AW-1:0]),
+                 .phy_in_srcaddr        (phy_in_srcaddr[AW-1:0]),
+                 .phy_in_data           (phy_in_data[RW-1:0]),
+                 .phy_out_ready         (phy_out_ready),
                  .regs_in_valid         (regs2cb_valid),         // Templated
                  .regs_in_cmd           (regs2cb_cmd[CW-1:0]),   // Templated
                  .regs_in_dstaddr       (regs2cb_dstaddr[AW-1:0]), // Templated
                  .regs_in_srcaddr       (regs2cb_srcaddr[AW-1:0]), // Templated
-                 .regs_in_data          (regs2cb_data[DW-1:0]),  // Templated
+                 .regs_in_data          (regs2cb_data[RW-1:0]),  // Templated
                  .regs_out_ready        (cb2regs_ready));        // Templated
 
    //########################
@@ -331,10 +312,10 @@ module lumi
     */
 
    lumi_tx #(.TARGET(TARGET),
-	      .IOW(IOW),
-              .CW(CW),
-              .AW(AW),
-	      .DW(DW))
+	     .IOW(IOW),
+             .CW(CW),
+             .AW(AW),
+	     .DW(DW))
    lumi_tx(/*AUTOINST*/
            // Outputs
            .umi_req_in_ready    (udev_req_ready),        // Templated
