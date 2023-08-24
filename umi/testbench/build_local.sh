e7f3b9abcf6c990444f7ae2f651a4edb6004a7d7
volkamir <amir@zeroasic.com>
2023-08-23 21:40:09 -0700

    update old tb

--- umi/testbench/build_local.sh
+++ umi/testbench/build_local.sh
@@ -1,13 +1,13 @@
 \rm -f a.out
 
-iverilog tb_umi_fifo.v \
-	 -y . \
-	 -y ../rtl \
-	 -I . \
-	 -I ../rtl \
-	 -y ../../submodules/lambdalib/stdlib/rtl \
-	 -y ../../submodules/lambdalib/vectorlib/rtl \
-	 -y ../../submodules/lambdalib/ramlib/rtl
+iverilog tb_umi_fifo_flex.v \
+         -y . \
+         -y ../rtl \
+         -I . \
+         -I ../rtl \
+         -y ../../submodules/lambdalib/stdlib/rtl \
+         -y ../../submodules/lambdalib/vectorlib/rtl \
+         -y ../../submodules/lambdalib/ramlib/rtl
 
 #./a.out
 #./a.out +MEMHFILE=./hello.memh
