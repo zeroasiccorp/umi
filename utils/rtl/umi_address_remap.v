4545e30a5d21e506c813c2ee9c64111e79bfe1a4
azaidy <ali@zeroasic.com>
2023-09-14 04:37:21 +0000

    Parameterize ID start bit

--- utils/rtl/umi_address_remap.v
+++ utils/rtl/umi_address_remap.v
@@ -9,6 +9,13 @@
  *
  * Documentation:
  * This module remaps UMI transactions across different memory regions.
+ * The module introduces a new parameter called IDSB which denotes the bit at
+ * which the chip ID starts in a umi address. This parameter can also be
+ * considered as the per ebrick/clink address space. For example, in a 64 bit
+ * address space, IDSB is 40 - along with IDW = 16, this mean the chip ID
+ * resides between IDSB + IDW - 1 to IDSB i.e. bits 55:40. Additionally, one
+ * can also infer that the addresses within a clink/umi connected device is
+ * 40 bits wide i.e. a memory space of 1 TiB.
  *****************************************************************************/
 
 `timescale 1ns / 1ps
@@ -19,6 +26,7 @@ module umi_address_remap #(
     parameter AW    = 64,   // address width
     parameter DW    = 128,  // data width
     parameter IDW   = 16,   // id width
+    parameter IDSB  = 40,   // id start bit - bit 40 in 64 bit address space
     parameter NMAPS = 8     // number of remaps
 )
 (
@@ -27,9 +35,9 @@ module umi_address_remap #(
     input  [IDW*NMAPS-1:0]  old_row_col_address,
     input  [IDW*NMAPS-1:0]  new_row_col_address,
 
-    input  [39:0]           set_dstaddress_offset,
-    input  [39:0]           set_dstaddress_high,
-    input  [39:0]           set_dstaddress_low,
+    input  [IDSB-1:0]       set_dstaddress_offset,
+    input  [IDSB-1:0]       set_dstaddress_high,
+    input  [IDSB-1:0]       set_dstaddress_low,
 
     input                   umi_in_valid,
     input  [CW-1:0]         umi_in_cmd,
@@ -60,11 +68,12 @@ module umi_address_remap #(
     reg [IDW-1:0] dstaddr_upper;
 
     always @(*) begin
-        if (umi_in_dstaddr[55:40] == chipid) begin
+        if (umi_in_dstaddr[(IDSB+IDW-1):IDSB] == chipid) begin
             dstaddr_upper = chipid;
         end
         else begin
-            case (umi_in_dstaddr[55:40])
+            // FIXME: Parameterize this
+            case (umi_in_dstaddr[(IDSB+IDW-1):IDSB])
                 old_row_col_address_unpack[0] : dstaddr_upper = new_row_col_address_unpack[0];
                 old_row_col_address_unpack[1] : dstaddr_upper = new_row_col_address_unpack[1];
                 old_row_col_address_unpack[2] : dstaddr_upper = new_row_col_address_unpack[2];
@@ -73,24 +82,26 @@ module umi_address_remap #(
                 old_row_col_address_unpack[5] : dstaddr_upper = new_row_col_address_unpack[5];
                 old_row_col_address_unpack[6] : dstaddr_upper = new_row_col_address_unpack[6];
                 old_row_col_address_unpack[7] : dstaddr_upper = new_row_col_address_unpack[7];
-                default                       : dstaddr_upper = umi_in_dstaddr[55:40];
+                default                       : dstaddr_upper = umi_in_dstaddr[(IDSB+IDW-1):IDSB];
             endcase
         end
     end
 
-    reg [39:0] dstaddr_lower;
+    reg [IDSB-1:0]  dstaddr_lower;
 
     always @(*) begin
-        if ((umi_in_dstaddr[39:0] >= set_dstaddress_low) &
-            (umi_in_dstaddr[39:0] <= set_dstaddress_high))
-            dstaddr_lower = umi_in_dstaddr[39:0] - set_dstaddress_offset;
+        if ((umi_in_dstaddr[IDSB-1:0] >= set_dstaddress_low) &
+            (umi_in_dstaddr[IDSB-1:0] <= set_dstaddress_high))
+            dstaddr_lower = umi_in_dstaddr[IDSB-1:0] - set_dstaddress_offset;
         else
-            dstaddr_lower = umi_in_dstaddr[39:0];
+            dstaddr_lower = umi_in_dstaddr[IDSB-1:0];
     end
 
     assign umi_out_valid    = umi_in_valid;
     assign umi_out_cmd      = umi_in_cmd;
-    assign umi_out_dstaddr  = {umi_in_dstaddr[63:56], dstaddr_upper, dstaddr_lower};
+    assign umi_out_dstaddr  = ((IDSB+IDW) < AW) ?
+                              {umi_in_dstaddr[AW-1:IDSB+IDW], dstaddr_upper, dstaddr_lower} :
+                              {dstaddr_upper, dstaddr_lower};
     assign umi_out_srcaddr  = umi_in_srcaddr;
     assign umi_out_data     = umi_in_data;
     assign umi_in_ready     = umi_out_ready;
