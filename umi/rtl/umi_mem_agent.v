21bb492915276fe9576b3cfa82f6f188dae0b2a9
aolofsson <andreas@zeroasic.com>
2023-11-21 22:23:15 -0500

    Fixing UMI RTL license headers - Removing authors, clear from commit history, friendlier - Adding apache license per file, better for copying

--- umi/rtl/umi_mem_agent.v
+++ umi/rtl/umi_mem_agent.v
@@ -1,15 +1,27 @@
-/******************************************************************************
- * Function:  UMI memory agent
- * Author:    Amir Volk
+/*******************************************************************************
+ * Copyright 2020 Zero ASIC Corporation
  *
- * Copyright (c) 2023 Zero ASIC Corporation
- * This code is licensed under Apache License 2.0 (see LICENSE for details)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This block is implementing a simple memory array for use in simulation
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Limitation - transaction cannot cross the DW boundary (need to be split at the request side)
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- ****************************************************************************/
+ * ----
+ *
+ * Documentation:
+ * - Implements a simple memory array for use in simulation
+ *
+ * Known limitation/bugs:
+ * - Transaction cannot cross the DW boundary
+ *
+ ******************************************************************************/
 
 module umi_mem_agent
   #(parameter DW = 256,           // umi packet width
