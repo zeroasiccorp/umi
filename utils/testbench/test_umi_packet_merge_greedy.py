557c222dffdb916c20cb8de24f67545b6a1a1f34
Peter Gadfort <gadfort@zeroasic.com>
2024-02-27 08:15:22 -0500

    update testbenches to use python lambdalib

--- utils/testbench/test_umi_packet_merge_greedy.py
+++ utils/testbench/test_umi_packet_merge_greedy.py
@@ -2,12 +2,10 @@
 
 # Copyright (C) 2023 Zero ASIC
 
-import random
-import time
-import numpy as np
 from pathlib import Path
 from argparse import ArgumentParser
-from switchboard import SbDut, UmiTxRx, delete_queue, verilator_run, binary_run, random_umi_packet
+from switchboard import SbDut, UmiTxRx, delete_queue, verilator_run, random_umi_packet
+from lambdalib import lambdalib
 
 
 def build_testbench(topo="2d"):
@@ -26,14 +24,16 @@ def build_testbench(topo="2d"):
     else:
         raise ValueError('Invalid topology')
 
+    dut.use(lambdalib)
+    dut.add('option', 'ydir', 'lambdalib/ramlib/rtl', package='lambdalib')
+    dut.add('option', 'ydir', 'lambdalib/stdlib/rtl', package='lambdalib')
+    dut.add('option', 'ydir', 'lambdalib/padring/rtl', package='lambdalib')
+    dut.add('option', 'ydir', 'lambdalib/vectorlib/rtl', package='lambdalib')
+
     dut.input('testbench_umi_packet_merge_greedy.cc')
     for option in ['ydir', 'idir']:
         dut.add('option', option, EX_DIR / 'umi' / 'rtl')
         dut.add('option', option, EX_DIR / 'utils' / 'rtl')
-        dut.add('option', option, EX_DIR / 'submodules' / 'lambdalib' / 'lambdalib' / 'ramlib' / 'rtl')
-        dut.add('option', option, EX_DIR / 'submodules' / 'lambdalib' / 'lambdalib' / 'stdlib' / 'rtl')
-        dut.add('option', option, EX_DIR / 'submodules' / 'lambdalib' / 'lambdalib' / 'padring' / 'rtl')
-        dut.add('option', option, EX_DIR / 'submodules' / 'lambdalib' / 'lambdalib' / 'vectorlib' / 'rtl')
 
     # Verilator configuration
     vlt_config = EX_DIR / 'utils' / 'testbench' / 'config.vlt'
