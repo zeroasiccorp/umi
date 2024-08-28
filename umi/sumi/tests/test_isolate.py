e146c16554d3feab8a3e39d8c83dfb81bbc982e8
azaidy <zaidy.ali@gmail.com>
2024-08-28 17:46:38 -0400

    Move tests for crossbar, fifo, and fifo_flex. Add test for umi_isolate.
    Fix bug in umi_isolate where srcaddr was being used instead of data

---
+++ umi/sumi/tests/test_isolate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2024 Zero ASIC
+# This code is licensed under Apache License 2.0 (see LICENSE for details)
+
+import pytest
+import random
+import numpy as np
+from switchboard import UmiTxRx, umi_loopback
+from umi import sumi
+
+
+def test_isolate(sumi_dut, valid_mode, ready_mode):
+
+    # launch the simulation
+    sumi_dut.simulate(plusargs=[('valid_mode', valid_mode), ('ready_mode', ready_mode)])
+
+    # instantiate TX and RX queues.  note that these can be instantiated without
+    # specifying a URI, in which case the URI can be specified later via the
+    # "init" method
+
+    host = UmiTxRx("host2dut_0.q", "dut2host_0.q", fresh=True)
+
+    print("### Statring test ###")
+
+    umi_loopback(host, 1000, max_bytes=32)
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', '-q', __file__])
