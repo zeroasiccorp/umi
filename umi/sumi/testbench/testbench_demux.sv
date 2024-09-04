452558103f5798b2b1c67bffde63cd95f369cc6d
azaidy <zaidy.ali@gmail.com>
2024-09-04 15:55:02 -0400

    Add test for demux

---
+++ umi/sumi/testbench/testbench_demux.sv
@@ -0,0 +1,161 @@
+/*******************************************************************************
+ * Copyright 2024 Zero ASIC Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ----
+ *
+ * Documentation:
+ * - Simple umi demux testbench
+ *
+ ******************************************************************************/
+
+module testbench (
+`ifdef VERILATOR
+    input clk
+`endif
+);
+
+`include "switchboard.vh"
+
+    localparam M  = 4;
+    localparam DW = 256;
+    localparam CW = 32;
+    localparam AW = 64;
+
+    localparam PERIOD_CLK   = 10;
+
+`ifndef VERILATOR
+    // Generate clock for non verilator sim tools
+    reg clk;
+
+    initial
+        clk  = 1'b0;
+    always #(PERIOD_CLK/2) clk = ~clk;
+`endif
+
+    // Initialize UMI
+    integer valid_mode, ready_mode;
+
+    initial begin
+        /* verilator lint_off IGNOREDRETURN */
+        if (!$value$plusargs("valid_mode=%d", valid_mode)) begin
+            valid_mode = 2;  // default if not provided as a plusarg
+        end
+
+        if (!$value$plusargs("ready_mode=%d", ready_mode)) begin
+            ready_mode = 2;  // default if not provided as a plusarg
+        end
+        /* verilator lint_on IGNOREDRETURN */
+    end
+
+    wire [M-1:0]    select;
+
+    wire            umi_in_valid;
+    wire [CW-1:0]   umi_in_cmd;
+    wire [AW-1:0]   umi_in_dstaddr;
+    wire [AW-1:0]   umi_in_srcaddr;
+    wire [DW-1:0]   umi_in_data;
+    wire            umi_in_ready;
+
+    wire [M-1:0]    umi_out_valid;
+    wire [M*CW-1:0] umi_out_cmd;
+    wire [M*AW-1:0] umi_out_dstaddr;
+    wire [M*AW-1:0] umi_out_srcaddr;
+    wire [M*DW-1:0] umi_out_data;
+    wire [M-1:0]    umi_out_ready;
+
+    ///////////////////////////////////////////
+    // Host side umi agents
+    ///////////////////////////////////////////
+
+    genvar          i;
+
+    umi_rx_sim #(
+        .VALID_MODE_DEFAULT (2),
+        .DW                 (DW)
+    ) umi_rx_i (
+        .clk        (clk),
+
+        .valid      (umi_in_valid),
+        .cmd        (umi_in_cmd),
+        .dstaddr    (umi_in_dstaddr),
+        .srcaddr    (umi_in_srcaddr),
+        .data       (umi_in_data),
+        .ready      (umi_in_ready)
+    );
+
+    assign select = umi_in_dstaddr[40+:M];
+
+    initial begin
+        `ifndef VERILATOR
+            #1;
+        `endif
+        umi_rx_i.init("client2rtl_0.q");
+        umi_rx_i.set_valid_mode(valid_mode);
+    end
+
+    generate
+    for(i = 0; i < M; i = i + 1) begin: demux_out
+        umi_tx_sim #(
+            .READY_MODE_DEFAULT (2),
+            .DW                 (DW)
+        ) umi_tx_i (
+            .clk        (clk),
+
+            .valid      (umi_out_valid[i]),
+            .cmd        (umi_out_cmd[i*CW+:CW]),
+            .dstaddr    (umi_out_dstaddr[i*AW+:AW]),
+            .srcaddr    (umi_out_srcaddr[i*AW+:AW]),
+            .data       (umi_out_data[i*DW+:DW]),
+            .ready      (umi_out_ready[i])
+        );
+
+        initial begin
+            demux_out[i].umi_tx_i.init($sformatf("rtl2client_%0d.q", i));
+            demux_out[i].umi_tx_i.set_ready_mode(ready_mode);
+        end
+    end
+    endgenerate
+
+    // UMI Demux
+    umi_demux #(
+        .M  (M),
+        .DW (DW),
+        .CW (CW),
+        .AW (AW)
+    ) umi_demux_i (
+        .select             (select),
+
+        .umi_in_valid       (umi_in_valid),
+        .umi_in_cmd         (umi_in_cmd),
+        .umi_in_dstaddr     (umi_in_dstaddr),
+        .umi_in_srcaddr     (umi_in_srcaddr),
+        .umi_in_data        (umi_in_data),
+        .umi_in_ready       (umi_in_ready),
+
+        .umi_out_valid      (umi_out_valid),
+        .umi_out_cmd        (umi_out_cmd),
+        .umi_out_dstaddr    (umi_out_dstaddr),
+        .umi_out_srcaddr    (umi_out_srcaddr),
+        .umi_out_data       (umi_out_data),
+        .umi_out_ready      (umi_out_ready)
+    );
+
+    // waveform dump
+    `SB_SETUP_PROBES
+
+    // auto-stop
+    auto_stop_sim auto_stop_sim_i (.clk(clk));
+
+endmodule
---
+++ umi/sumi/tests/test_demux.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2024 Zero ASIC
+# This code is licensed under Apache License 2.0 (see LICENSE for details)
+
+import pytest
+import multiprocessing
+import numpy as np
+from switchboard import UmiTxRx, random_umi_packet, delete_queue
+
+
+def umi_send(umi_q, n, ports, seed):
+
+    np.random.seed(seed)
+
+    tee = [UmiTxRx(f'tee_{x}.q', '') for x in range(ports)]
+
+    for count in range(n*ports):
+        dstport = np.random.randint(0, ports)
+        dstaddr = (2**8)*np.random.randint(0, 2**32) + (2**(40+dstport))
+        srcaddr = (2**8)*np.random.randint(0, 2**32) + (2**(40+ports))
+        txp = random_umi_packet(dstaddr=dstaddr, srcaddr=srcaddr)
+        print(f"Sending #{count} cmd: 0x{txp.cmd:08x} srcaddr: 0x{srcaddr:08x} "
+              f"dstaddr: 0x{dstaddr:08x} to port {dstport}")
+        # send the packet to both simulation and local queues
+        umi_q.send(txp)
+        tee[dstport].send(txp)
+
+
+def test_demux(sumi_dut, random_seed, sb_umi_valid_mode, sb_umi_ready_mode):
+    n = 100
+    ports = 4
+
+    for x in range(ports):
+        delete_queue(f'tee_{x}.q')
+
+    # instantiate TX and RX queues.  note that these can be instantiated without
+    # specifying a URI, in which case the URI can be specified later via the
+    # "init" method
+
+    umi = UmiTxRx('client2rtl_0.q', '', fresh=True)
+    tee = [UmiTxRx('', f'tee_{x}.q') for x in range(ports)]
+    recvq = [UmiTxRx('', f'rtl2client_{x}.q', fresh=True) for x in range(ports)]
+
+    # launch the simulation
+    sumi_dut.simulate(
+            plusargs=[('valid_mode', sb_umi_valid_mode),
+                      ('ready_mode', sb_umi_ready_mode)])
+
+    print("### Starting test ###")
+
+    send_proc = multiprocessing.Process(target=umi_send,
+                                        args=(umi, n, ports, random_seed,))
+
+    send_proc.start()
+
+    recv_queue = [[] for i in range(ports)]
+    send_queue = [[] for i in range(ports)]
+
+    nrecv = 0
+    nsend = 0
+    while (nrecv < ports*n) or (nsend < ports*n):
+        for i in range(ports):
+            rxp = recvq[i].recv(blocking=False)
+            if rxp is not None:
+                if nrecv >= ports*n:
+                    print(f'Unexpected packet received {nrecv}')
+                    raise Exception(f'Unexpected packet received {nrecv}')
+                else:
+                    recv_src = (rxp.srcaddr >> 40)
+                    print(f"port {i} receiving srcaddr: 0x{rxp.srcaddr:08x} "
+                          f"dstaddr: 0x{rxp.dstaddr:08x} src: {recv_src} #{nrecv}")
+                    recv_queue[i].append(rxp)
+                    nrecv += 1
+
+        for i in range(ports):
+            txp = tee[i].recv(blocking=False)
+            if txp is not None:
+                if nsend >= ports*n:
+                    raise Exception('Unexpected packet sent')
+                else:
+                    send_dst = (txp.dstaddr >> 40)
+                    send_queue[i].append(txp)
+                    nsend += 1
+
+    # join Tx sender
+    send_proc.join()
+
+    for i in range(ports):
+        if len(send_queue[i]) != len(recv_queue[i]):
+            print(f"packets sent: {len(send_queue[i])} packets received: {len(recv_queue[i])}")
+        assert len(send_queue[i]) == len(recv_queue[i])
+        for txp, rxp in zip(send_queue[i], recv_queue[i]):
+            assert txp == rxp
+        print(f"Received {len(recv_queue[i])} packets at port {i}")
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', '-q', __file__])
