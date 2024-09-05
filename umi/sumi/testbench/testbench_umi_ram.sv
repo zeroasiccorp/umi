16f29b4b04c0d11eb5145009c7d071a918bdba06
azaidy <zaidy.ali@gmail.com>
2024-09-05 16:35:11 -0400

    Make umi_ram and regif tests - pytest compatible

--- umi/sumi/testbench/testbench_umi_ram.sv
+++ umi/sumi/testbench/testbench_umi_ram.sv
@@ -23,8 +23,12 @@
 `default_nettype none
 
 module testbench (
-                  input clk
-                  );
+`ifdef VERILATOR
+    input clk
+`endif
+);
+
+`include "switchboard.vh"
 
    parameter integer N=5;
    parameter integer CW=32;
@@ -33,8 +37,18 @@ module testbench (
    parameter integer CTRLW=8;
    parameter integer RAMDEPTH=512;
 
+   localparam PERIOD_CLK   = 10;
+
+`ifndef VERILATOR
+    // Generate clock for non verilator sim tools
+    reg clk;
+
+    initial
+        clk  = 1'b0;
+    always #(PERIOD_CLK/2) clk = ~clk;
+`endif
+
    reg                  nreset;
-   reg                  go;
 
    wire [N-1:0]         udev_req_valid;
    wire [N*CW-1:0]      udev_req_cmd;
@@ -52,6 +66,21 @@ module testbench (
 
    wire [CTRLW-1:0]  sram_ctrl = 8'b0;
    wire [1:0]        mode = 2'b10;
+
+   // Initialize valid and ready modes
+   integer valid_mode, ready_mode;
+
+   initial begin
+      /* verilator lint_off IGNOREDRETURN */
+      if (!$value$plusargs("valid_mode=%d", valid_mode)) begin
+         valid_mode = 2;  // default if not provided as a plusarg
+      end
+
+      if (!$value$plusargs("ready_mode=%d", ready_mode)) begin
+         ready_mode = 2;  // default if not provided as a plusarg
+      end
+      /* verilator lint_on IGNOREDRETURN */
+   end
 
    ///////////////////////////////////////////
    // Host side umi agents
@@ -83,6 +112,16 @@ module testbench (
                        .data(udev_resp_data[i*DW+:DW]),
                        .ready(udev_resp_ready[i])
                        );
+
+        initial begin
+           `ifndef VERILATOR
+              #1;
+           `endif
+           UMI_AGENTS_GEN[i].host_umi_rx_i.init($sformatf("host2dut_%0d.q", i));
+           UMI_AGENTS_GEN[i].host_umi_rx_i.set_valid_mode(valid_mode);
+           UMI_AGENTS_GEN[i].host_umi_tx_i.init($sformatf("dut2host_%0d.q", i));
+           UMI_AGENTS_GEN[i].host_umi_tx_i.set_ready_mode(ready_mode);
+        end
    end
    endgenerate
 
@@ -115,71 +154,23 @@ module testbench (
              .udev_req_data       (udev_req_data[N*DW-1:0]),
              .udev_resp_ready     (udev_resp_ready[N-1:0]));
 
-            // Initialize UMI
-   integer valid_mode, ready_mode;
-
-   initial begin
-      /* verilator lint_off IGNOREDRETURN */
-      if (!$value$plusargs("valid_mode=%d", valid_mode)) begin
-         valid_mode = 2;  // default if not provided as a plusarg
-      end
-
-      if (!$value$plusargs("ready_mode=%d", ready_mode)) begin
-         ready_mode = 2;  // default if not provided as a plusarg
-      end
-
-      UMI_AGENTS_GEN[0].host_umi_rx_i.init("host2dut_0.q");
-      UMI_AGENTS_GEN[0].host_umi_rx_i.set_valid_mode(valid_mode);
-      UMI_AGENTS_GEN[1].host_umi_rx_i.init("host2dut_1.q");
-      UMI_AGENTS_GEN[1].host_umi_rx_i.set_valid_mode(valid_mode);
-      UMI_AGENTS_GEN[2].host_umi_rx_i.init("host2dut_2.q");
-      UMI_AGENTS_GEN[2].host_umi_rx_i.set_valid_mode(valid_mode);
-      UMI_AGENTS_GEN[3].host_umi_rx_i.init("host2dut_3.q");
-      UMI_AGENTS_GEN[3].host_umi_rx_i.set_valid_mode(valid_mode);
-      UMI_AGENTS_GEN[4].host_umi_rx_i.init("host2dut_4.q");
-      UMI_AGENTS_GEN[4].host_umi_rx_i.set_valid_mode(valid_mode);
-
-      UMI_AGENTS_GEN[0].host_umi_tx_i.init("dut2host_0.q");
-      UMI_AGENTS_GEN[0].host_umi_tx_i.set_ready_mode(ready_mode);
-      UMI_AGENTS_GEN[1].host_umi_tx_i.init("dut2host_1.q");
-      UMI_AGENTS_GEN[1].host_umi_tx_i.set_ready_mode(ready_mode);
-      UMI_AGENTS_GEN[2].host_umi_tx_i.init("dut2host_2.q");
-      UMI_AGENTS_GEN[2].host_umi_tx_i.set_ready_mode(ready_mode);
-      UMI_AGENTS_GEN[3].host_umi_tx_i.init("dut2host_3.q");
-      UMI_AGENTS_GEN[3].host_umi_tx_i.set_ready_mode(ready_mode);
-      UMI_AGENTS_GEN[4].host_umi_tx_i.init("dut2host_4.q");
-      UMI_AGENTS_GEN[4].host_umi_tx_i.set_ready_mode(ready_mode);
-      /* verilator lint_on IGNOREDRETURN */
-   end
-
-   // VCD
-
+   // reset
    initial
      begin
         nreset   = 1'b0;
-        go       = 1'b0;
      end // initial begin
 
-   // Bring up reset and the go signal on the first clock cycle
+   // Bring up reset on the first clock cycle
    always @(negedge clk)
      begin
         nreset <= nreset | 1'b1;
-        go <= 1'b1;
      end
 
-   // control block
-   initial
-     begin
-        if ($test$plusargs("trace"))
-          begin
-             $dumpfile("testbench.fst");
-             $dumpvars(0, testbench);
-          end
-     end
+    // waveform dump
+    `SB_SETUP_PROBES
 
-   // auto-stop
-
-   auto_stop_sim auto_stop_sim_i (.clk(clk));
+    // auto-stop
+    auto_stop_sim auto_stop_sim_i (.clk(clk));
 
 endmodule
 // Local Variables:
