5eea7c2c4859eef2896008462e8d23ad466b1347
Andreas Olofsson <andreas@zeroasic.com>
2025-01-28 16:50:45 -0500

    Lint fix

--- umi/sumi/tests/test_regif.py
+++ umi/sumi/tests/test_regif.py
@@ -7,6 +7,7 @@ import pytest
 import numpy as np
 from switchboard import UmiTxRx
 
+
 def test_regif(sumi_dut, random_seed, sb_umi_valid_mode, sb_umi_ready_mode):
 
     np.random.seed(random_seed)
