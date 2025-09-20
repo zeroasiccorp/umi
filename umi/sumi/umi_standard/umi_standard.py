b6d477ca76a0db57bcd5b925e10358f0786bac1e
Andreas Olofsson <andreas@zeroasic.com>
2025-09-20 09:02:35 -0400

    Fixing name to be consistent

--- umi/sumi/umi_standard/umi_standard.py
+++ umi/sumi/umi_standard/umi_standard.py
@@ -4,11 +4,11 @@ from siliconcompiler.design import Design
 class Standard(Design):
     def __init__(self):
 
-        name = 'standard'
+        name = 'umi_standard'
         super().__init__(name)
         self.set_dataroot('root', dirname(abspath(__file__)))
         self.add_idir('include', fileset='rtl')
 
 if __name__ == "__main__":
     d = Standard()
-    d.write_fileset("standard.f", fileset="rtl")
+    d.write_fileset("umi_standard.f", fileset="rtl")
