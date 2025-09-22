ac4e69335640bb81e99e9f284d9d2a432044eeb6
Andreas Olofsson <andreas@zeroasic.com>
2025-09-22 06:49:12 -0400

    First batch of new umi python setup files

---
+++ umi/sumi/umi_decode/umi_decode.py
@@ -0,0 +1,11 @@
+from umi.sumi.sumi import Sumi
+
+class Decode(Sumi):
+    def __init__(self):
+        name = 'umi_decode'
+        sources = 'rtl/umi_decode.v'
+        super().__init__(name, sources)
+
+if __name__ == "__main__":
+    d = Decode()
+    d.write_fileset("umi_decode.f", fileset="rtl")
