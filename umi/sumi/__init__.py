d6aa00b478bc7019bcf44c6467af6a4ad2d1ca18
Andreas Olofsson <andreas@zeroasic.com>
2025-09-22 18:17:37 -0400

    Expaning user API

--- umi/sumi/__init__.py
+++ umi/sumi/__init__.py
@@ -1,25 +1,33 @@
 from .umi_arbiter.umi_arbiter import Arbiter
+from .umi_crossbar.umi_crossbar import Crossbar
 from .umi_decode.umi_decode import Decode
+from .umi_endpoint.umi_endpoint import Endpoint
 from .umi_fifo.umi_fifo import Fifo
 from .umi_fifoflex.umi_fifoflex import FifoFlex
+from .umi_memagent.umi_memagent import MemAgent
 from .umi_mux.umi_mux import Mux
 from .umi_mux2.umi_mux2 import Mux2
 from .umi_pack.umi_pack import Pack
 from .umi_pipeline.umi_pipeline import Pipeline
 from .umi_ram.umi_ram import RAM
 from .umi_regif.umi_regif import Regif
+from .umi_switch.umi_switch import Switch
 from .umi_tester.umi_tester import Tester
 from .umi_unpack.umi_unpack import Unpack
 
 __all__ = ['Arbiter',
+           'Crossbar',
            'Decode',
+           'Endpoint',
            'Fifo',
            'FifoFlex',
+           'MemAgent',
            'Mux',
            'Mux2',
            'Pack',
            'Pipeline',
            'RAM',
            'Regif',
+           'Switch',
            'Tester',
            'Unpack']
