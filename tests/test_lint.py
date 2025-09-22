3e9a5e6801d2a567177e82ef1c0f28c45d2bcfbf
Andreas Olofsson <andreas@zeroasic.com>
2025-09-22 18:18:36 -0400

    Updating lint script with new/better methodology

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -1,41 +1,16 @@
-from siliconcompiler import Chip
-from siliconcompiler.flows import lintflow
-import glob
 import pytest
+import siliconcompiler as sc
+from siliconcompiler.flows import lintflow
 import umi
-import shutil
-from pathlib import Path
 
 
-@pytest.fixture
-def slang_chip():
-    chip = Chip('lint')
-    chip.use(lintflow, tool='slang')
-
-    chip.use(umi)
-    chip.set('option', 'flow', 'lintflow')
-
-    return chip
+def lint(design):
+    proj = sc.Project(design)
+    proj.add_fileset("rtl")
+    proj.set_flow(lintflow.LintFlow())
+    return proj.run()
 
 
-@pytest.fixture
-def slang():
-    if shutil.which('slang') is None:
-        pytest.skip('slang is not installed')
-
-
-def __filelist():
-    return glob.glob(str(Path(umi.__file__).parent / '**' / 'rtl' / '*.v'))
-
-
-@pytest.mark.parametrize('file', __filelist())
-def test_lint_slang(slang_chip, file):
-    slang_chip.set('option', 'entrypoint', Path(file).stem)
-
-    if slang_chip.get('option', 'entrypoint') == 'umi_packet_merge_greedy':
-        pytest.skip(reason='File is outdaded and will be cleaned up later')
-
-    slang_chip.input(file)
-    slang_chip.run()
-
-    assert slang_chip.get('record', 'toolexitcode', step='lint', index='0') == 0
+@pytest.mark.parametrize("name", umi.sumi.__all__)
+def test_lint_sumi(name):
+    assert lint(getattr(umi.sumi, name)())
