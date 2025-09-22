4074ee759d36bd871cae5591d11dd14508088378
Andreas Olofsson <andreas@zeroasic.com>
2025-09-22 06:47:33 -0400

    Initial init for sumi package

--- umi/sumi/__init__.py
+++ umi/sumi/__init__.py
@@ -1,22 +1,31 @@
+from .umi_standard.umi_standard import Standard
+from .umi_pack.umi_pack import Pack
+#from .umi_unpack.umi_unpack import Unpack
+#from .umi_decode.umi_decode import Decode
+#from .umi_tester.umi_tester import Tester
+#from .umi_fifo.umi_fifo import Fifo
+#from .umi_fifoflex.umi_fifoflex import FifoFlex
+#from .umi_regif.umi_regif import Regif
+#from .umi_mux.umi_mux import Mux
+#from .umi_mux2.umi_mux2 import Mux2
+#from .umi_ram.umi_ram import Ram
+#from .umi_arbiter.umi_arbiter import Arbiter
+#from .umi_pipline.umi_pipline import Pipeline
 
-from .la_and2.la_and2 import And2
+##################################################
+# Public API
+##################################################
 
-
-from siliconcompiler import Library
-from lambdalib import auxlib, ramlib, vectorlib
-
-
-def setup():
-    lib = Library("sumi", package=("umi", "python://umi"), auto_enable=True)
-
-    lib.add("option", "idir", "sumi/rtl")
-    lib.add("option", "ydir", "sumi/rtl")
-
-    lib.add("option", "idir", "utils/rtl")
-    lib.add("option", "ydir", "utils/rtl")
-
-    lib.use(auxlib)
-    lib.use(ramlib)
-    lib.use(vectorlib)
-
-    return lib
+__all__ = ['Sumi',
+           'Standard',
+           'Pack',
+           'Unpack',
+           'Decode',
+           'Tester',
+           'Fifo',
+           'FifoFlex',
+           'Regif',
+           'Mux',
+           'Ram',
+           'Arbiter',
+           'Pipeline']
