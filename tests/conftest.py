bc6d47e22bce475796735f423972fc4219cfe7a7
Andreas Olofsson <andreas@zeroasic.com>
2025-09-22 12:55:21 -0400

    Adding basic sumi test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,14 @@
 import pytest
 import os
 
-
 @pytest.fixture(autouse=True)
 def test_wrapper(tmp_path):
     '''
     Fixture that automatically runs each test in a test-specific temporary
     directory to avoid clutter.
     '''
-
     topdir = os.getcwd()
     os.chdir(tmp_path)
-
     # Run the test.
     yield
-
     os.chdir(topdir)
