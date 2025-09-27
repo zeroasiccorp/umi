50f30d6fad2479e4bff49a05e5e8554395affb16
Andreas Olofsson <andreas@zeroasic.com>
2025-09-26 20:58:43 -0400

    Adding init file for adapters

---
+++ umi/adapters/__init__.py
@@ -0,0 +1,10 @@
+# Design objects
+#from .axil2umi.axil2umi import AXIL2UMI
+#from .umi2apb.umi2apb import UMI2APB
+#from .umi2axil.umi2axil import UMI2AXIL
+
+
+#__all__ = ['AXIL2UMI',
+#           'UMI2APB',
+#           'UMI2AXIL'
+#           ]
