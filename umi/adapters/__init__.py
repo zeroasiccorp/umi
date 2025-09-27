41ce04940dd33bd8bea02532ac81b76a4bf57353
Andreas Olofsson <andreas@zeroasic.com>
2025-09-26 21:05:59 -0400

    Expanding adapteres api

--- umi/adapters/__init__.py
+++ umi/adapters/__init__.py
@@ -1,10 +1,12 @@
 # Design objects
-#from .axil2umi.axil2umi import AXIL2UMI
-#from .umi2apb.umi2apb import UMI2APB
-#from .umi2axil.umi2axil import UMI2AXIL
+from .axil2umi.axil2umi import AXIL2UMI
+from .umi2axil.umi2axil import UMI2AXIL
+from .umi2apb.umi2apb import UMI2APB
+from .tl2umi.tl2umi import TL2UMI
+from .umi2tl.umi2tl import UMI2TL
 
-
-#__all__ = ['AXIL2UMI',
-#           'UMI2APB',
-#           'UMI2AXIL'
-#           ]
+__all__ = ['AXIL2UMI',
+           'UMI2APB',
+           'UMI2AXIL',
+           'AXIL2UMI',
+           'UMI2APB']
