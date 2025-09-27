2f5192e5bf3376cdbc6e18dae8fe10cf2debfdc6
Andreas Olofsson <andreas@zeroasic.com>
2025-09-26 21:29:07 -0400

    Adding new LUMI class

---
+++ umi/lumi/lumi.py
@@ -0,0 +1,36 @@
+from umi.common import UMI
+from lambdalib.auxlib import Drsync
+from lambdalib.auxlib import Dsync
+from lambdalib.auxlib import Rsync
+from lambdalib.ramlib import Asyncfifo
+from umi.sumi import FifoFlex
+from umi.sumi import Crossbar
+from umi.sumi import Unpack
+from umi.sumi import Pack
+from umi.sumi import Decode
+from umi.sumi import Regif
+from umi.sumi import Mux
+
+class LUMI(UMI):
+    def __init__(self):
+        super().__init__('lumi',
+                         files=['rtl/lumi.v',
+                                'rtl/lumi_crossbar.v',
+                                'rtl/lumi_regs.v',
+                                'rtl/lumi_tx.v',
+                                'rtl/lumi_tx_ready.v',
+                                'rtl/lumi_rx.v',
+                                'rtl/lumi_rx_ready.v'],
+                         idirs=['rtl'],
+                         deps=[Mux(),
+                               FifoFlex(),
+                               Crossbar(),
+                               Regif(),
+                               Asyncfifo(),
+                               Rsync(),
+                               Dsync()])
+
+
+if __name__ == "__main__":
+    d = LUMI()
+    d.write_fileset(f"{d.name}.f", fileset="rtl")
