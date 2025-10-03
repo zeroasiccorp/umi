919aeb6f418e9ed8b410e702ded22a91936db1a2
Andreas Olofsson <andreas@zeroasic.com>
2025-10-03 14:20:20 -0400

    Adding standard to package

--- umi/__init__.py
+++ umi/__init__.py
@@ -1,10 +1,12 @@
 from umi import sumi
 from umi import lumi
 from umi import adapters
+from umi.common import Standard
 
 __version__ = "0.3.0"
 
 __all__ = [
+    "Standard",
     "sumi",
     "lumi",
     "adapters"
