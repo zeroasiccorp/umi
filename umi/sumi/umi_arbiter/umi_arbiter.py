d399db9fa4da8d98bfe5e57d08a6d2356ee2cd29
Andreas Olofsson <andreas@zeroasic.com>
2025-10-23 08:36:07 -0400

    Including priority encoder as inline - Putting prioroty in lambda veclib was inappropriate - Logic is simple enough that we don't need a function - May find another home for circuits like this, but not sure... - Single header versus complex library stack question...

--- umi/sumi/umi_arbiter/umi_arbiter.py
+++ umi/sumi/umi_arbiter/umi_arbiter.py
@@ -1,12 +1,10 @@
 from umi.common import UMI
-from lambdalib.veclib import Vpriority
-
 
 class Arbiter(UMI):
     def __init__(self):
         super().__init__('umi_arbiter',
                          files=['rtl/umi_arbiter.v'],
-                         deps=[Vpriority()])
+                         deps=[])
 
 
 if __name__ == "__main__":
