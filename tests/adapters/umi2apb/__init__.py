bc5083c7808d93dca6fe4bb93be82d22f2385c65
AndyOja <lucky.anders.12@gmail.com>
2026-01-16 19:56:37 +0000

    resolved umi2apb bugs, added tests

---
+++ tests/adapters/umi2apb/__init__.py
@@ -0,0 +1,6 @@
+# Import tests so cocotb can discover them
+from adapters.umi2apb.test_basic_WR import *
+from adapters.umi2apb.test_full_throughput import *
+from adapters.umi2apb.test_random_stimulus import *
+from adapters.umi2apb.test_posted_write import *
+from adapters.umi2apb.test_backpressure import *
---
+++ tests/sumi_monitor.py
@@ -0,0 +1,47 @@
+from cocotb.types import LogicArray
+from cocotb.triggers import RisingEdge
+
+from cocotb_bus.monitors import BusMonitor
+
+from sumi import SumiCmd, SumiTransaction
+
+
+class SumiMonitor(BusMonitor):
+
+    _signals = [
+        "valid",
+        "cmd",
+        "dstaddr",
+        "srcaddr",
+        "data",
+        "ready"
+    ]
+    _optional_signals = []
+
+    def __init__(self, entity, name, clock, **kwargs):
+        BusMonitor.__init__(self, entity, name, clock, **kwargs)
+        self.addr_width = len(self.bus.dstaddr)
+
+    async def _monitor_recv(self):
+        clk_re = RisingEdge(self.clock)
+
+        def valid_handshake() -> bool:
+            return bool(self.bus.valid.value) and bool(self.bus.ready.value)
+
+        while True:
+            await clk_re
+
+            if self.in_reset:
+                continue
+
+            if valid_handshake():
+                sumi_cmd: SumiCmd = SumiCmd.from_int(int(self.bus.cmd.value))
+                data: LogicArray = self.bus.data.value
+                data = data[(((int(sumi_cmd.len)+1) << (int(sumi_cmd.size)))*8)-1:0]
+                self._recv(SumiTransaction(
+                    cmd=sumi_cmd,
+                    da=int(self.bus.dstaddr.value) if self.bus.dstaddr.value.is_resolvable else None,
+                    sa=int(self.bus.srcaddr.value) if self.bus.srcaddr.value.is_resolvable else None,
+                    data=data.to_bytes(byteorder="little") if data.is_resolvable else None,
+                    addr_width=self.addr_width
+                ))
