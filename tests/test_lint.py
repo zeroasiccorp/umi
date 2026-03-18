6453c74e63a2fb392610aa5de9419e6b9abb5a84
Andreas Olofsson <andreas@zeroasic.com>
2026-03-18 17:24:28 -0400

    Lint cleanup

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -13,8 +13,7 @@ _all_modules = [
 
 @pytest.mark.parametrize("mod,name", _all_modules)
 def test_slang_lint(mod, name):
-    design =getattr(mod,name)()
-    top = design.get_topmodule("rtl")
+    design = getattr(mod, name)()
     proj = sc.Lint(design)
     proj.add_fileset("rtl")
     proj.set_flow(lintflow.LintFlow())
@@ -24,8 +23,7 @@ def test_slang_lint(mod, name):
 @pytest.mark.eda
 @pytest.mark.parametrize("mod,name", _all_modules)
 def test_verilator_lint(mod, name):
-    design =getattr(mod,name)()
-    top = design.get_topmodule("rtl")
+    design = getattr(mod, name)()
     proj = sc.Lint(design)
     proj.add_fileset("rtl")
     proj.set_flow(lintflow.LintFlow(tool="verilator"))
