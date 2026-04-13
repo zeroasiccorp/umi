5c735bc963f44d210ea63ac7b85dbba697653a82
Andreas Olofsson <andreas@zeroasic.com>
2026-04-13 19:35:15 -0400

    Hopefully final fix for umi_monitor...

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -24,6 +24,7 @@ def test_slang_lint(mod, name):
 @pytest.mark.parametrize("mod,name", _all_modules)
 def test_verilator_lint(mod, name):
     design = getattr(mod, name)()
+    design.add_define('SIMULATION', fileset='rtl')
     proj = sc.Lint(design)
     proj.add_fileset("rtl")
     proj.set_flow(lintflow.LintFlow(tool="verilator"))
